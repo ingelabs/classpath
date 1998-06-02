c2decd0f2948c3b9b0528770fb9b137de05691f9
Paul Fisher <rao@gnu.org>
1998-06-02 19:33:31 +0000

    Added java.lang.Number

---
+++ java/lang/Number.java
@@ -0,0 +1,18 @@
+package java.lang;
+
+public abstract class Number implements Serializable
+{
+  public abstract int intValue();
+  public abstract long longValue();
+  public abstract float floatValue();
+  public abstract double doubleValue();
+  
+  public byte byteValue() {
+    return (byte) intValue();
+  }
+
+  public short shortValue() {
+    return (short) intValue();
+  }
+}
+
