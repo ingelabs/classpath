17ec4524bc72617244cf3f28e0b1b2d84fcb716a
Paul Fisher <rao@gnu.org>
1998-06-03 04:47:29 +0000

    Added java.lang.Compiler (does nothing).

---
+++ java/lang/Compiler.java
@@ -0,0 +1,21 @@
+package java.lang;
+
+public final class Compiler {
+  public static boolean compileClass(Class clazz) {
+    return false;
+  }
+
+  public static boolean compileClasses(String string) {
+    return false;
+  }
+
+  public static Object command(Object any) {
+    return null;
+  }
+
+  public static void enable() {
+  }
+
+  public static void disable() {
+  }
+}
