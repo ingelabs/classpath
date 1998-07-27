4fced0309157ad27a6511c6c78483ec11938d0e0
Geoff Berry <gcb@gnu.org>
1998-07-27 23:31:05 +0000

    Implemented all non-abstract methods.

--- java/util/AbstractMap.java
+++ java/util/AbstractMap.java
@@ -1,7 +1,8 @@
 /////////////////////////////////////////////////////////////////////////////
 // AbstractMap.java -- Abstract implementation of most of Map
 //
-// Copyright (c) 1998 by Stuart Ballard (stuart.ballard@mcmail.com)
+// Copyright (c) 1998 by Stuart Ballard (stuart.ballard@mcmail.com),
+//                       Geoff Berry (gcb@cs.duke.edu)
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU Library General Public License as published
@@ -18,24 +19,251 @@
 /////////////////////////////////////////////////////////////////////////////
 
 // TO DO:
-// ~ Everything.
+// comments
+// test suite
 
 package java.util;
 
 public abstract class AbstractMap implements Map {
-  public void clear() {}
-  public boolean containsKey(Object key) {}
-  public boolean containsValue(Object value) {}
+
+  public void clear()
+  {
+    entries().clear();
+  }
+
+  public boolean containsKey( Object key )
+  {
+    Object k;
+    Iterator entries = entries().iterator();
+  
+    while( entries.hasNext() )
+    {
+      k = ((Map.Entry)entries.next()).getKey();
+      if( key == null ? k == null : key.equals( k ) )
+	return true;
+    }
+
+    return false;
+  }
+
+  public boolean containsValue( Object value )
+  {
+    Object v;
+    Iterator entries = entries().iterator();
+  
+    while( entries.hasNext() )
+    {
+      v = ((Map.Entry)entries.next()).getValue();
+      if( value == null ? v == null : value.equals( v ) )
+	return true;
+    }
+
+    return false; 
+  }
+
   public abstract Set entries();
-  public boolean equals(Object o) {}
-  public Object get(Object key) {}
-  public int hashCode() {}
-  public boolean isEmpty() {}
-  public Set keySet() {}
-  public Object put(Object key, Object value) {}
-  public void putAll(Map m) {}
-  public Object remove(Object key) {}
-  public int size() {}
-  public String toString() {}
-  public Collection values() {}
+
+  public boolean equals( Object o )
+  {
+    if( this == o )
+      return true;
+    
+    if( o == null || !( o instanceof Map ) )
+      return false;
+    
+    Map m = (Map)o;
+    if( m.size() != size() )
+      return false;
+    
+    Object key, value1, value2;
+    Map.Entry entry;
+    Iterator entries = entries().iterator();
+    while( entries.hasNext() )
+    {
+      entry = (Map.Entry)entries.next();
+      key = entry.getKey();
+      value1 = entry.getValue();
+      value2 = m.get( key );
+      
+      if( !( ( value1 == null && value2 == null )
+	     || value1.equals( value2 ) ) )
+	return false;
+    }
+
+    return true;    
+  }
+
+  public Object get( Object key )
+  {
+    Object k;
+    Map.Entry entry;
+    Iterator entries = entries().iterator();
+  
+    while( entries.hasNext() )
+    {
+      entry = (Map.Entry)entries.next();
+      k = entry.getKey();
+      if( key == null ? k == null : key.equals( k ) )
+	return entry.getValue();
+    }
+
+    return null;
+  }
+
+  public int hashCode()
+  {
+    int hashcode = 0;
+    Iterator entries = entries().iterator();
+  
+    while( entries.hasNext() )
+      hashcode += entries.next().hashCode();
+
+    return hashcode;
+  }
+
+  public boolean isEmpty()
+  {
+    return size() == 0;
+  }
+
+  public Set keySet()
+  {
+    if( this.keySet == null )
+    {
+      this.keySet =
+	new AbstractSet()
+	{
+	  public int size()
+          {
+	    return AbstractMap.this.size();
+	  }
+	  
+	  public Iterator iterator()
+          {
+	    return new Iterator()
+            {
+	      Iterator map_iterator = AbstractMap.this.entries().iterator();
+	      
+	      public boolean hasNext()
+              {
+		return this.map_iterator.hasNext();
+	      }
+
+	      public Object next()
+              {
+		return ((Map.Entry)this.map_iterator.next()).getKey();
+	      }
+	      
+	      public void remove()
+              {
+		this.map_iterator.remove();
+	      }
+	    };
+	  }
+	};
+    }
+    
+    return this.keySet;    
+  }
+
+  public Object put( Object key, Object value )
+  {
+    throw new UnsupportedOperationException();
+  }
+
+  public void putAll( Map m )
+  {
+    Map.Entry entry;
+    Iterator entries = m.entries().iterator();
+    while( entries.hasNext() )
+    {
+      entry = (Map.Entry)entries.next();
+      put( entry.getKey(), entry.getValue() );
+    }
+  }
+
+  public Object remove( Object key )
+  {
+    Object k, value;
+    Map.Entry entry;
+    Iterator entries = entries().iterator();
+  
+    while( entries.hasNext() )
+    {
+      entry = (Map.Entry)entries.next();
+      k = entry.getKey();
+      if( key == null ? k == null : key.equals( k ) )
+      {
+	value = entry.getValue();
+	entries.remove();
+	return value;
+      }
+    }
+
+    return null;    
+  }
+
+  public int size()
+  {
+    return entries().size();
+  }
+
+  public String toString()
+  {
+    Map.Entry entry;
+    String rep = "AbstractMap< ";
+    Iterator entries = entries().iterator();
+  
+    while( entries.hasNext() )
+    {
+      entry = (Map.Entry)entries.next();
+      rep += entry.getKey() + " -> " + entry.getValue() + ", ";
+    }
+
+    return rep.substring( 0, rep.length() - 2 ) + " >";
+  }
+
+  public Collection values()
+  {
+    if( this.valueCollection == null )
+    {
+      this.valueCollection =
+	new AbstractCollection()
+	{
+	  public int size()
+          {
+	    return AbstractMap.this.size();
+	  }
+	  
+	  public Iterator iterator()
+          {
+	    return new Iterator()
+            {
+	      Iterator map_iterator = AbstractMap.this.entries().iterator();
+	      
+	      public boolean hasNext()
+              {
+		return this.map_iterator.hasNext();
+	      }
+
+	      public Object next()
+              {
+		return ((Map.Entry)this.map_iterator.next()).getValue();
+	      }
+	      
+	      public void remove()
+              {
+		this.map_iterator.remove();
+	      }
+	    };
+	  }
+	};
+    }
+    
+    return this.valueCollection;
+  }
+
+
+  private Collection valueCollection = null;
+  private Set keySet = null;
 }
