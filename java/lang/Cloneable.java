0a29b8746f653b230a2bd3ccf5a56374800b298c
Paul Fisher <rao@gnu.org>
1998-08-10 04:20:13 +0000

    Add API docs.

--- java/lang/Cloneable.java
+++ java/lang/Cloneable.java
@@ -1,3 +1,13 @@
 package java.lang;
 
+/**
+ * This interface should be implemented by classes wishing to
+ * support or override Object.clone().  Cloning an object generally
+ * results in a deep copy of that object.  If Object.clone() is called
+ * on an object which does not implement the Cloneable interface,
+ * a CloneNotSupportedException will be thrown.
+ *
+ * @see java.lang.Object#clone
+ * @see java.lang.CloneNotSupportedException
+ */
 public interface Cloneable { }
