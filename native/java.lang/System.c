fd753a20bf97d155367f1c0422f5c78312bae292
John Keiser <shalom@gnu.org>
1998-10-09 19:05:30 +0000

    Stole Japhar's implementation of System.currentTimeMillis(), courtesy Chris Toshok.

--- native/java.lang/System.c
+++ native/java.lang/System.c
@@ -1,5 +1,5 @@
 #include "System.h"
-#include <time.h>
+#include <sys/time.h>
 
 /*
  * Class:     java_lang_System
@@ -41,5 +41,16 @@ JNIEXPORT void JNICALL Java_java_lang_System_setErr
  */
 JNIEXPORT jlong JNICALL Java_java_lang_System_currentTimeMillis
   (JNIEnv * env, jclass thisClass) {
-	return time(NULL) * 1000;
+  /* Note: this implementation copied directly from Japhar's, by Chris Toshok. */
+  jlong result;
+  struct timeval tp;
+
+  if (gettimeofday(&tp, NULL) == -1)
+    (*env)->FatalError(env, "gettimeofday call failed.");
+
+  result = (jlong)tp.tv_sec;
+  result *= 1000;
+  result += (tp.tv_usec / 1000);
+
+  return result;
 }
