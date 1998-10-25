6cec43f98e3f5f6ce95b12d9ee68c76cc9327aa6
Paul Fisher <rao@gnu.org>
1998-10-25 03:12:14 +0000

    Finished implementing JDK1.2 java.lang.Math.

--- java/lang/Math.java
+++ java/lang/Math.java
@@ -1,6 +1,15 @@
 package java.lang;
 
+import java.util.Random;
+
 public final class Math {
+
+  static {
+    System.loadLibrary("java_lang_Math");
+  }
+
+  static Random rand;
+
   public static final double E = 2.7182818284590452354; 
   public static final double PI = 3.14159265358979323846;
 
@@ -58,5 +67,43 @@ public final class Math {
     if (a == 0.0d && b == 0.0d)	// return +0.0d, if a or b is +0.0d
       return ((Double.doubleToLongBits(a) >> 63) == 0) ? a : b;
     return (a > b) ? a : b;
+  }
+
+  public native static double sin(double a);
+  public native static double cos(double a);
+  public native static double tan(double a);
+  public native static double asin(double a);
+  public native static double acos(double a);
+  public native static double atan(double a);
+  public native static double atan2(double y, double x);
+  public native static double exp(double a);
+  public native static double log(double a);
+  public native static double sqrt(double a);
+  public native static double pow(double a, double b);
+  public native static double IEEEremainder(double x, double y);
+  public native static double ceil(double a);
+  public native static double floor(double a);
+  public native static double rint(double a);
+
+  public static int round(float a) {
+    return (int)floor(a + 0.5f);
+  }
+
+  public static int round(double a) {
+    return (int)floor(a + 0.5d);
+  }
+
+  public static synchronized double random() {
+    if (rand == null)
+      rand = new Random();
+    return rand.nextDouble();
+  }
+
+  public static double toRadians(double degrees) {
+    return degrees * 0.017453292519943295; /* (degrees * (PI/180)) */
+  }
+
+  public static double toDegrees(double rads) {
+    return rads / 0.017453292519943295; /* (rads / (PI/180)) */
   }
 }
