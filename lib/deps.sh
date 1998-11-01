b79f167107bafec9701d9e27b067b732a480a976
Brian Jones <cbj@gnu.org>
1998-11-01 02:46:39 +0000

    include the right directories in search

--- lib/deps.sh
+++ lib/deps.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
-find ../vm ../gnu ../java -type f -print | grep "\.java\$" > classes
+find ../vm ../gnu/java ../gnu/test ../gnu/vm ../java/beans ../java/io ../java/lang ../java/math ../java/net ../java/security ../java/util  -type f -print | grep "\.java\$" > classes
 ../../JavaDeps-2.0.1/jdeps -b "\$(JAVAC)" -d lib -f classes -v > .deps
