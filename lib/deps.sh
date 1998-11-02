fdbd6857434c8407357d211138b562f5bce06030
Brian Jones <cbj@gnu.org>
1998-11-02 15:47:55 +0000

    set directory to classes correctly in .deps

--- lib/deps.sh
+++ lib/deps.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
-find ../vm ../gnu/java ../gnu/test ../gnu/vm ../java/beans ../java/io ../java/lang ../java/math ../java/net ../java/security ../java/util  -type f -print | grep "\.java\$" > classes
-../../JavaDeps-2.0.1/jdeps -b "\$(JAVAC)" -d lib -f classes -v > .deps
+find ../vm ../gnu/java ../gnu/test ../gnu/vm ../java/beans ../java/io ../java/lang ../java/math ../java/net ../java/security ../java/util ../java/text -type f -print | grep "\.java\$" > classes
+../../JavaDeps-2.0.1/jdeps --native -b "\$(JAVAC)" -d . -f classes -v > .deps
