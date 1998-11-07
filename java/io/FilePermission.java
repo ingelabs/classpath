28b97c769614d6068addee0f7c4abec5da76c414
John Keiser <shalom@gnu.org>
1998-11-07 00:24:00 +0000

    Initial checkin.

---
+++ java/io/FilePermission.java
@@ -0,0 +1,279 @@
+/*
+ * java.lang.FilePermission: part of the Java Class Libraries project.
+ * Copyright (C) 1998 Free Software Foundation
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA  02111-1307, USA.
+ */
+
+package java.io;
+
+/**
+ ** FilePermission represents permission on files and directories.
+ ** You specify a file (or set of files), and the desired set of actions on
+ ** that file.<P>
+ **
+ ** When you specify a file, you may use <code>*</code> as a wildcard
+ ** to specify "all files in a directory," and you may use <code>-</code>
+ ** to specify "all files in a directory or in any of its subdirectories."
+ ** If you do not place a file separator at the beginning of the path, it is
+ ** relative to the current directory.  You may not add a <code>*</code> or a
+ ** <code>-</code> to the end of a filename.  You may only place it after a
+ ** file separator character.  <code>&lt;&lt;ALL FILES&gt;&gt;</code> is a
+ ** special value that signifies all files on all filesystems.<P>
+ **
+ ** Examples from a Unix system (all path and filenames are system dependent):<P>
+ ** <UL>
+ ** <LI><code>/home/shalom/filename.txt</code> (access to the specific file)</LI>
+ ** <LI><code>/home/shalom</code> (specifies access to the directory itself)</LI>
+ ** <LI><code>/home/shalom/</code> (also specifies access to the directory itself)</LI>
+ ** <LI><code>/home/shalom/*</code> (everything in <code>/home/shalom</code>)</LI>
+ ** <LI><code>/home/shalom/-</code> (everything in <code>/home/shalom</code> and subdirectories)</LI>
+ ** <LI><code>mysubdir/*</code> (everything in <code>&lt;current directory&gt;/mysubdir</code>)</LI>
+ ** <LI><code>mysubdir/-</code> (everything in <code>&lt;current directory&gt;/mysubdir</code> and subdirectories)</LI>
+ ** <LI><code>*</code> (everything in current directory)</LI>
+ ** <LI><code>-</code> (everything in current directory and subdirectories)</LI>
+ ** <LI><code>&lt;&lt;ALL FILES&gt;&gt;</code> (all files)</LI>
+ ** </UL>
+ **
+ ** The supported actions are <code>read</code>, <code>write</code>,
+ ** <code>execute</code> and <code>delete</code>.  You may specify multiple
+ ** actions in the actions string, separated by commas.<P>
+ **
+ ** <STRONG>Where various permissions are checked in the core system:</STRONG><P>
+ ** <TABLE>
+ ** <TR><TH>Action</TH><TH>Location Called</TH></TR>
+ ** <TR><TD><code>read</code></TD>    <TD><code></code></TD></TR>
+ ** <TR><TD><code>write</code></TD>   <TD><code></code></TD></TR>
+ ** <TR><TD><code>execute</code></TD> <TD><code>Runtime.exec()</code>, <code>System.checkExec()</code></TD></TR>
+ ** <TR><TD><code>delete</code></TD>  <TD><code>File.delete()</code>, <code>System.checkDelete()</code></TD></TR>
+ ** </TABLE>
+ **
+ ** @specnote Do <code>dir/*</code> and <code>dir/-</code> imply access to dir
+ **           itself or just files and folders under it?  I am assuming
+ **           <code>-</code> does and <code>*</code> does not.
+ ** @XXX I did not implement newPermissionCollection() because no optimizations
+ **      could be made that I could think of.  Sun did.  This needs to be looked
+ **      over carefully to see what is different.
+ **
+ ** @author John Keiser
+ ** @version 1.1.0, Nov 5 1998
+ **/
+
+public class FilePermission extends Permission implements Serializable {
+	private boolean usingPerms = false;
+	private boolean readPerm = false;
+	private boolean writePerm = false;
+	private boolean executePerm = false;
+	private boolean deletePerm = false;
+	private String actionsString;
+
+	private void cachePerms() {
+		// While race conditions could occur, they don't matter at all.
+
+		String action;
+		int i = actionsString.indexOf(',');
+		int startI = 0;
+		while(i != -1) {
+			action = actionsString.substring(startI,i);
+			if(action.equals("read"))
+				readPerm = true;
+			else if(action.equals("write"))
+				writePerm = true;
+			else if(action.equals("execute"))
+				executePerm = true;
+			else if(action.equals("delete"))
+				deletePerm = true;
+
+			startI = i+1;
+			i = actionsString.indexOf(',',startI);
+		}
+
+		action = actionsString.substring(startI);
+		if(action.equals("read"))
+			readPerm = true;
+		else if(action.equals("write"))
+			writePerm = true;
+		else if(action.equals("execute"))
+			executePerm = true;
+		else if(action.equals("delete"))
+			deletePerm = true;
+	}
+
+	/** Create a new FilePermission.
+	 ** @param pathExpression an expression specifying the paths this
+	 **        permission represents.
+	 ** @param actionsString a comma-separated list of the actions this
+	 **        permission represents.
+	 ** @XXX what to do when the file string is malformed?
+	 **/
+	public void FilePermission(String pathExpression, String actionsString) {
+		super(pathExpression);
+		this.actionsString = actionsString;
+	}
+
+	/** Get the actions this FilePermission supports.
+	 ** @return the String representing the actions this FilePermission supports.
+	 **/
+	public String getActions() {
+		return actionsString;
+	}
+
+	/** Get the hash code for this Object.<P>
+	 ** FilePermission's hash code is calculated as the exclusive or of the target
+	 ** String's hash code and the action String's hash code.
+	 ** @specnote Sun did not specify how to calculate the hash code; I made this up.
+	 ** @return the hash code for this Object.
+	 **/
+	public int hashCode() {
+		return getName().hashCode() ^ actionsString.hashCode();
+	}
+
+	/** Check two FilePermissions for semantic equality.
+	 ** Two FilePermissions are exactly equivalent if they have identical path
+	 ** expressions and have exactly the same access permissions.
+	 ** @param o the Object to compare to.
+	 ** @return whether the Objects are semantically equivalent.
+	 **/
+	public boolean equals(Object o) {
+		if(!(o instanceof FilePermission))
+			return false;
+		FilePermission p = (FilePermission)o;
+		if(!usingPerms)
+			cachePerms();
+		if(!p.usingPerms)
+			p.cachePerms();
+
+		/* Compare names, taking into account if they refer to a
+		 * directory and one has a separator and the other does not.
+		 */
+		if(f1.charAt(f1.length()) == File.separatorChar) {
+			if(f2.charAt(f2.length()) == File.separatorChar) {
+				if(!f2.equals(f1))
+					return false;
+			} else {
+				if(!f2.equals(f1.substring(0,f1.length()-1)))
+					return false;
+			}
+		} else {
+			if(f2.charAt(f2.length()) == File.separatorChar) {
+				if(!f1.equals(f2.substring(0,f2.length()-1)))
+					return false;
+			} else {
+				if(!f1.equals(f2))
+					return false;
+			}
+		}
+		return readPerm == p.readPerm && writePerm == p.writePerm && executePerm == p.executePerm && deletePerm == p.deletePerm;
+	}
+
+	/** Check to see if this permission implies another.
+	 ** Permission A implies permission B if these things are all true:
+	 ** <OL>
+	 ** <LI>A and B are both FilePermissions.</LI>
+	 ** <LI>All possible files in B are included in A (possibly more are in A).</LI>
+	 ** <LI>All actions B supports, A also supports.</LI>
+	 ** </OL>
+	 ** @param p the Permission to compare against.
+	 ** @return whether this Permission implies p
+	 **/
+	public boolean implies(Permission p) {
+		FilePermission fp;
+		if(!(p instanceof FilePermission))
+			return false;
+		fp = (FilePermission)p;
+
+		String f1 = getName();
+		String f2 = fp.getName();
+		if(f1.charAt(0) != File.separatorChar) {
+			f1 = GET_CURRENT_DIRECTORY + f1;
+		}
+		if(f2.charAt(0) != File.separatorChar) {
+			f2 = GET_CURRENT_DIRECTORY + f2;
+		}
+
+		String sub1, sub2a, sub2b;
+		switch(f1.charAt(f1.length() - 1)) {
+			case '*':
+				sub1 = f1.substring(0,f1.length() - 1); // chop off "*"
+				if(f2.length() <= sub1.length()) {
+					/* If it's smaller, there is no way it could be part of this directory.
+					 * If it's the same (or length - 1), it could be the same directory but
+					 * specifies access to the directory rather than the files in it.
+					 */
+					return false;
+				} else if(f2.charAt(sub1.length() - 1) == File.separatorChar) {
+					/* Make sure the part before the "/" is the same */
+					if(!f2.substring(0,sub1.length()).equals(sub1))
+						return false;
+					/* Make sure there are no subdirectories specified underneath this one */
+					String sub2 = f2.substring(sub1.length()+1);
+					if(f2.substring(sub1.length()+1).indexOf(File.separatorChar) != -1)
+						return false;
+				} else {
+					/* Obviously not equal: f2 is either not a directory or is not
+					 * the same directory (its name continues further than we want)
+					 */
+					return false;
+				}
+				break;
+			case '-':
+				sub1 = f1.substring(0,f1.length() - 2); // chop off "/-"
+				if(f2.length() < sub1.length()) {
+					/* If it's smaller, there is no way it could be part of this directory. */
+					return false;
+				} else if(f2.length() > sub1.length() && f2.charAt(sub1.length()) != File.separatorChar) {
+					return false;
+				} else if(!f2.substring(0,sub1.length()).equals(sub1))
+					return false;
+				}
+				break;
+			case File.separatorChar:
+				if(f2.charAt(f2.length()) == File.separatorChar) {
+					if(!f2.equals(f1))
+						return false;
+				} else {
+					if(!f2.equals(f1.substring(0,f1.length()-1)))
+						return false;
+				}
+				break;
+			default:
+				if(f2.charAt(f2.length()) == File.separatorChar) {
+					if(!f1.equals(f2.substring(0,f2.length()-1)))
+						return false;
+				} else {
+					if(!f1.equals(f2))
+						return false;
+				}
+				break;
+		}
+
+		if(!usePerms)
+			cachePerms();
+		if(!fp.usePerms)
+			fp.cachePerms();
+
+		if(readPerm && !fp.readPerm)
+			return false;
+		if(writePerm && !fp.writePerm)
+			return false;
+		if(executePerm && !fp.executePerm)
+			return false;
+		if(deletePerm && !fp.deletePerm)
+			return false;
+
+		return true;
+	}
+}
