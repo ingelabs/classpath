4607d38a1a4b3bd972bbd60a3b27d3c29bfdc832
John Keiser <shalom@gnu.org>
1998-11-23 05:13:55 +0000

    Modified deps.sh to work with a .omit file which allows the build to exclude certain files (most notably java.security, for now).  Each line in standard.omit is a line to be passed to grep -v to screen out files. This should be extended later so that when we want to, we can create different builds of Classpath (Personal Java, Server Java, etc.)

--- lib/deps.sh
+++ lib/deps.sh
@@ -1,4 +1,3 @@
 #! /bin/sh
-
-find ../vm ../gnu/java ../gnu/test ../gnu/vm ../java/beans ../java/io ../java/lang ../java/math ../java/net ../java/security ../java/util ../java/text -type f -print | grep "\.java\$" > classes
+gen-classlist.sh standard
 ../../JavaDeps-2.0.1/jdeps --native -b "\$(JAVAC)" -d . -f classes -v > .deps
