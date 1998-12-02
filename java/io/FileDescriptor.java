1f2a0573e069b9ae592250d7cdf26e14f4d0d21a
Aaron M. Renn <arenn@urbanophile.com>
1998-12-02 02:46:33 +0000

    Added static initializer to load native library

--- java/io/FileDescriptor.java
+++ java/io/FileDescriptor.java
@@ -57,6 +57,11 @@ public static final FileDescriptor out = new FileDescriptor(1);
   */
 public static final FileDescriptor err = new FileDescriptor(2);
 
+static
+{
+  System.loadLibrary("javaio");
+}
+
 /*************************************************************************/
 
 /**
