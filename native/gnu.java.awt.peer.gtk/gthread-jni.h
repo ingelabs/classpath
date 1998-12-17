1f22f4a3b3fe8e12c5b784438ad5acbffd5d5e39
Paul Fisher <rao@gnu.org>
1998-12-17 14:24:22 +0000

    Initial commit of new threading routines for GLIB.

---
+++ native/gnu.java.awt.peer.gtk/gthread-jni.h
@@ -0,0 +1,10 @@
+#ifndef __GTHREADJNI_H__
+#define __GTHREADJNI_H__
+
+#include <jni.h>
+#include <glib.h>
+#include "gtkpeer.h"
+
+extern GThreadFunctions g_thread_jni_functions;
+
+#endif __GTHREADJNI_H__
