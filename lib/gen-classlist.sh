5ca811838d17f3881d4b65b4ee5dbd686769f2b4
Aaron M. Renn <arenn@urbanophile.com>
1998-12-19 07:28:46 +0000

    Remove ../vm/reference from class scan

--- lib/gen-classlist.sh
+++ lib/gen-classlist.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-find ../vm/reference ../java ../gnu -type f -print | grep '\.java$' > classes
+find ../java ../gnu -type f -print | grep '\.java$' > classes
 for filexp in `cat $1.omit` ; do { grep -v ${filexp} < classes > classes.new ; mv classes.new classes ; } ; done
