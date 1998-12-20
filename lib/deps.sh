bf0057922f16d6325925c16f90015ca091bbf869
Brian Jones <cbj@gnu.org>
1998-12-20 06:51:57 +0000

    generated by autoconf

--- lib/deps.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-gen-classlist.sh standard
-jdeps --native -b "\$(JAVAC)" -d . -f classes -v > .deps
--- lib/gen-classlist.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-find ../java ../gnu -type f -print | grep '\.java$' > classes
-for filexp in `cat $1.omit` ; do { grep -v ${filexp} < classes > classes.new ; mv classes.new classes ; } ; done
