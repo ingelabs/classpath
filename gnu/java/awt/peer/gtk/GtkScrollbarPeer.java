60f179930a0047a8e0547cb8b2738c93106b2228
James E. Blair <corvus@gnu.org>
1998-12-28 23:56:44 +0000

    Initial commit.

---
+++ gnu/java/awt/peer/gtk/GtkScrollbarPeer.java
@@ -0,0 +1,48 @@
+/*
+ * GtkScrollbarPeer.java -- Implements ScrollbarPeer with GTK
+ *
+ * Copyright (c) 1998 Free Software Foundation, Inc.
+ * Written by James E. Blair <corvus@gnu.org>
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Library General Public License as published 
+ * by the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later verion.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; if not, write to the Free Software Foundation
+ * Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA
+ */
+
+package gnu.java.awt.peer.gtk;
+import java.awt.*;
+import java.awt.peer.*;
+
+public class GtkScrollbarPeer extends GtkComponentPeer
+    implements ScrollbarPeer
+{
+
+  native void gtkScrollbarNew (int orientation, int value, int min, int max);
+  native public void setLineIncrement (int amount);
+  native public void setPageIncrement (int amount);
+  native public void setValues (int value, int size, int min, int max);
+
+  public GtkScrollbarPeer (Scrollbar s, ComponentPeer cp)
+  {
+    super (s);
+
+    gtkScrollbarNew ((s.getOrientation()==Scrollbar.HORIZONTAL)?1:0,
+		     s.getValue(),
+		     s.getMinimum(),
+		     s.getMaximum());
+
+    Point p=s.getLocation();
+    
+    gtkFixedPut (cp, p.x, p.y);
+  }
+}
---
+++ native/gnu.java.awt.peer.gtk/gtkscrollbarpeer.c
@@ -0,0 +1,107 @@
+/*
+ * gtkscrollbarpeer.c -- Native implementation of GtkScrollbarPeer
+ *
+ * Copyright (c) 1998 Free Software Foundation, Inc.
+ * Written by James E. Blair <corvus@gnu.org>
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Library General Public License as published 
+ * by the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later verion.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; if not, write to the Free Software Foundation
+ * Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA
+ */
+
+#include "gtkpeer.h"
+#include "GtkScrollbarPeer.h"
+
+JNIEXPORT void JNICALL 
+Java_gnu_java_awt_peer_gtk_GtkScrollbarPeer_gtkScrollbarNew
+    (JNIEnv *env, jobject obj, jint orientation, jint value, jint min, 
+     jint max)
+{
+  GtkWidget *sb;
+  GtkObject *adj;
+
+  gdk_threads_enter ();
+  adj = gtk_adjustment_new (value, min, max, 1, 10, 10);
+  if (orientation) 
+    sb = gtk_hscrollbar_new (GTK_ADJUSTMENT (adj));
+  else
+    sb = gtk_vscrollbar_new (GTK_ADJUSTMENT (adj));
+  connect_awt_hook (env, obj, sb, 4, 
+		    &(GTK_RANGE (sb)->trough),
+		    &(GTK_RANGE (sb)->slider),
+		    &(GTK_RANGE (sb)->step_forw),
+		    &(GTK_RANGE (sb)->step_back));
+  gdk_threads_leave ();
+
+  NSA_SET_PTR (env, obj, sb);
+}
+
+JNIEXPORT void JNICALL 
+Java_gnu_java_awt_peer_gtk_GtkScrollbarPeer_setLineIncrement
+    (JNIEnv *env, jobject obj, jint amount)
+{
+  void *ptr;
+  GtkAdjustment *adj;
+
+  ptr = NSA_GET_PTR (env, obj);
+
+  gdk_threads_enter ();
+
+  adj = GTK_RANGE(ptr)->adjustment;
+  adj->step_increment = amount;
+  gtk_adjustment_changed (adj);
+
+  gdk_threads_leave ();
+}
+
+JNIEXPORT void JNICALL 
+Java_gnu_java_awt_peer_gtk_GtkScrollbarPeer_setPageIncrement
+    (JNIEnv *env, jobject obj, jint amount)
+{
+  void *ptr;
+  GtkAdjustment *adj;
+
+  ptr = NSA_GET_PTR (env, obj);
+
+  gdk_threads_enter ();
+
+  adj = GTK_RANGE(ptr)->adjustment;
+  adj->page_increment = amount;
+  adj->page_size = amount;
+  gtk_adjustment_changed (adj);
+
+  gdk_threads_leave ();
+}
+
+JNIEXPORT void JNICALL 
+Java_gnu_java_awt_peer_gtk_GtkScrollbarPeer_setValues
+    (JNIEnv *env, jobject obj, jint value, int size, int min, int max)
+{
+  void *ptr;
+  GtkAdjustment *adj;
+
+  ptr = NSA_GET_PTR (env, obj);
+
+  gdk_threads_enter ();
+
+  adj = GTK_RANGE(ptr)->adjustment;
+  adj->lower = min;
+  adj->upper = max;
+  adj->value = value;
+  gtk_adjustment_changed (adj);
+
+  gdk_threads_leave ();
+}
+
+
+
