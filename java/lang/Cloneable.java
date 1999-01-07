19fc720cc06906b533c47b2811661bd607421859
Brian Jones <cbj@gnu.org>
1999-01-07 03:53:29 +0000

    copyright notice added

--- java/lang/Cloneable.java
+++ java/lang/Cloneable.java
@@ -1,3 +1,23 @@
+/*************************************************************************
+ * Cloneable.java -- Interface for implementing a method to override 
+ * Object.clone()comparaing objects to obtain an ordering
+ *
+ * Copyright (c) 1998 by Free Software Foundation, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Library General Public License as published 
+ * by the Free Software Foundation, version 2. (see COPYING.LIB)
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation
+ * Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA
+ *************************************************************************/
+
 package java.lang;
 
 /**
