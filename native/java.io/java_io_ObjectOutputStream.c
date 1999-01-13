9658be76a86a57892c914ef52072e9538a60ca39
Brian Jones <cbj@gnu.org>
1999-01-13 03:34:09 +0000

    remove /* within comment

--- native/java.io/java_io_ObjectOutputStream.c
+++ native/java.io/java_io_ObjectOutputStream.c
@@ -1,22 +1,22 @@
 /*************************************************************************
-/* java_io_ObjectOutputStream.c -- Native methods for
-/*                                 ObjectOutputStream class
-/*
-/* Copyright (c) 1998 by Free Software Foundation, Inc.
-/*
-/* This program is free software; you can redistribute it and/or modify
-/* it under the terms of the GNU Library General Public License as published 
-/* by the Free Software Foundation, version 2. (see COPYING.LIB)
-/*
-/* This program is distributed in the hope that it will be useful, but
-/* WITHOUT ANY WARRANTY; without even the implied warranty of
-/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-/* GNU General Public License for more details.
-/*
-/* You should have received a copy of the GNU General Public License
-/* along with this program; if not, write to the Free Software Foundation
-/* Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA
-/*************************************************************************/
+ * java_io_ObjectOutputStream.c -- Native methods for
+ *                                 ObjectOutputStream class
+ *
+ * Copyright (c) 1998 by Free Software Foundation, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Library General Public License as published 
+ * by the Free Software Foundation, version 2. (see COPYING.LIB)
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation
+ * Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA
+ *************************************************************************/
 
 //TODO: check exceptions
 //      comments
