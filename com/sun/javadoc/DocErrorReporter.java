affe80563b6118caeeb8eba935c6fd81c26f4d49
Aaron M. Renn <arenn@urbanophile.com>
1999-02-11 23:34:21 +0000

    Initial checkin of Javadoc API

---
+++ com/sun/javadoc/DocErrorReporter.java
@@ -0,0 +1,60 @@
+/*************************************************************************
+/* DocErrorReporter.java -- Log errors/warnings during doc generation.
+/*
+/* Copyright (c) 1999 Free Software Foundation, Inc.
+/* Written by Aaron M. Renn (arenn@urbanophile.com)
+/*
+/* This program is free software; you can redistribute it and/or modify
+/* it under the terms of the GNU General Public License as published 
+/* by the Free Software Foundation, either version 2 of the License, or
+/* (at your option) any later version.
+/*
+/* This program is distributed in the hope that it will be useful, but
+/* WITHOUT ANY WARRANTY; without even the implied warranty of
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/* GNU General Public License for more details.
+/*
+/* You should have received a copy of the GNU General Public License
+/* along with this program; if not, write to the Free Software Foundation
+/* Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA
+/*************************************************************************/
+
+package com.sun.javadoc;
+
+/**
+  * This interface provides a mechanism for a doclet to log messages
+  * during its run.
+  */
+public abstract interface DocErrorReporter
+{
+
+/**
+  * This method prints the specified error message.
+  *
+  * @param err The error message to print.
+  */
+public abstract void
+printError(String err);
+
+/*************************************************************************/
+
+/**
+  * This method prints the specified warning message.
+  *
+  * @param warn The warning message to print.
+  */
+public abstract void
+printWarning(String warn);
+
+/*************************************************************************/
+
+/**
+  * This method prints the specifed message.
+  *
+  * @param msg The message to print.
+  */
+public abstract void
+printNotice();
+
+} // interface DocErrorReporter
+
