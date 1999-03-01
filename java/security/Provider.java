d3125a0803daba34189d2073d9fecb220b8d48df
Geoff Berry <gcb@gnu.org>
1999-03-01 02:55:57 +0000

    (Provider): Make protected to match spec.

--- java/security/Provider.java
+++ java/security/Provider.java
@@ -1,23 +1,22 @@
-/*************************************************************************
-/* Provider.java -- Security provider information
 /*
-/* Copyright (c) 1998 Free Software Foundation, Inc.
-/* Written by Aaron M. Renn (arenn@urbanophile.com)
-/*
-/* This library is free software; you can redistribute it and/or modify
-/* it under the terms of the GNU Library General Public License as published 
-/* by the Free Software Foundation, either version 2 of the License, or
-/* (at your option) any later verion.
-/*
-/* This library is distributed in the hope that it will be useful, but
-/* WITHOUT ANY WARRANTY; without even the implied warranty of
-/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-/* GNU Library General Public License for more details.
-/*
-/* You should have received a copy of the GNU Library General Public License
-/* along with this library; if not, write to the Free Software Foundation
-/* Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA
-/*************************************************************************/
+ Provider.java -- Security provider information
+
+ Copyright (c) 1998 Free Software Foundation, Inc.
+ Written by Aaron M. Renn (arenn@urbanophile.com)
+
+ This library is free software; you can redistribute it and/or modify
+ it under the terms of the GNU Library General Public License as published 
+ by the Free Software Foundation, either version 2 of the License, or
+ (at your option) any later verion.
+
+ This library is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Library General Public License for more details.
+
+ You should have received a copy of the GNU Library General Public License
+ along with this library; if not, write to the Free Software Foundation
+ Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA */
 
 package java.security;
 
@@ -76,7 +75,7 @@ private double version;
   * @param version The version number for this <code>Provider</code>.
   * @param info A textual description of this provider.
   */
-public
+protected
 Provider(String name, double version, String info)
 {
   this.name = name;
