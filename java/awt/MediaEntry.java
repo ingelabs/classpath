f2bf77a298f33d0e62eadf73a654c5623c489157
Aaron M. Renn <arenn@urbanophile.com>
1999-12-30 03:16:08 +0000

    Committing these unfinished modules.  They don't compile but somebody might want to hack on them.  IIRC, the problem is that I can't figure out how to implement them based on the only spec I have - namely the Serialized Form listing in the Javadocs.

---
+++ java/awt/MediaEntry.java
@@ -0,0 +1,100 @@
+/*************************************************************************
+/* MediaEntry.java -- An entry in a MediaTracker
+/*
+/* Copyright (c) 1999 Free Software Foundation, Inc.
+/* Written by Aaron M. Renn (arenn@urbanophile.com)
+/*
+/* This library is free software; you can redistribute it and/or modify
+/* it under the terms of the GNU Library General Public License as published 
+/* by the Free Software Foundation, either version 2 of the License, or
+/* (at your option) any later verion.
+/*
+/* This library is distributed in the hope that it will be useful, but
+/* WITHOUT ANY WARRANTY; without even the implied warranty of
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/* GNU Library General Public License for more details.
+/*
+/* You should have received a copy of the GNU Library General Public License
+/* along with this library; if not, write to the Free Software Foundation
+/* Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA
+/*************************************************************************/
+
+package java.awt;
+
+/**
+  * This is an entry in the media tracker
+  *
+  * @author Aaron M. Renn (arenn@urbanophile.com)
+  */
+abstract class MediaEntry implements java.io.Serializable
+{
+
+private static final int LOADING = 1;
+private static final int ABORTED = 2;
+private static final int ERRORED = 4;
+private static final int COMPLETE = 8;
+private static final int LOADSTARTED = 16;
+private static final int DONE = 32;
+
+private MediaTracker tracker;
+private int ID;
+private int status;
+private boolean cancelled;
+private MediaEntry next;
+
+static MediaEntry
+insert(MediaEntry a, MediaEntry b)
+{
+  while (a.next != null)
+    a = a.next;
+
+  a.next = b;
+  return(b);
+}
+
+MediaEntry(MediaTracker tracker, int ID)
+{
+  this.tracker = tracker;
+  this.ID = ID;
+}
+
+public int
+getID()
+{
+  return(ID);
+}
+
+public int
+getStatus()
+{
+  return(status);
+}
+
+public void
+setStatus(int status)
+{
+  this.status = status;
+}
+
+public MediaEntry
+getNext()
+{
+  return(next);
+}
+
+public void
+cancel()
+{
+  cancelled = true;
+  if ((status == LOADING) || (status == LOADSTARTED))
+    setStatus(ABORTED);
+}
+
+abstract void
+startLoad();
+
+abstract Object
+getMedia();
+
+} // class MediaEntry 
+
