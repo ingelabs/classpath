384856ecfe4a24c9939936de10dfb516bbe0f97f
Paul Fisher <rao@gnu.org>
2000-03-16 20:45:52 +0000

    Change license to libgcc-like terms. Change DOS EOLs to Unix EOLs.

--- java/security/spec/DSAPublicKeySpec.java
+++ java/security/spec/DSAPublicKeySpec.java
@@ -1,20 +1,29 @@
 /* DSAPublicKeySpec.java --- DSA Public Key Specificaton class
-   
-  Copyright (c) 1999 by Free Software Foundation, Inc.
-  Written by Mark Benvenuto <ivymccough@worldnet.att.net>
+   Copyright (C) 1999 Free Software Foundation, Inc.
 
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU Library General Public License as published 
-  by the Free Software Foundation, version 2. (see COPYING.LIB)
+This file is part of GNU Classpath.
 
-  This program is distributed in the hope that it will be useful, but
-  WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
+GNU Classpath is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+ 
+GNU Classpath is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
 
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software Foundation
-  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307 USA. */
+You should have received a copy of the GNU General Public License
+along with GNU Classpath; see the file COPYING.  If not, write to the
+Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+02111-1307 USA.
+
+As a special exception, if you link this library with other files to
+produce an executable, this library does not by itself cause the
+resulting executable to be covered by the GNU General Public License.
+This exception does not however invalidate any other reasons why the
+executable file might be covered by the GNU General Public License. */
+
 
 package java.security.spec;
 import java.math.BigInteger;
@@ -29,65 +38,65 @@ import java.math.BigInteger;
 */
 public class DSAPublicKeySpec extends Object implements KeySpec 
 {
-private BigInteger y = null;
-private BigInteger p = null;
-private BigInteger q = null;
-private BigInteger g = null;
+  private BigInteger y = null;
+  private BigInteger p = null;
+  private BigInteger q = null;
+  private BigInteger g = null;
 
-/**
-	Constructs a new DSAPublicKeySpec with the specified y, p, q, and g.
+  /**
+     Constructs a new DSAPublicKeySpec with the specified y, p, q, and g.
 
-	@param y the public key
-	@param p the prime
-	@param q the sub-prime
-	@param g the base
-*/
-public DSAPublicKeySpec(BigInteger x, BigInteger p, BigInteger q, BigInteger g) 
-{
-	this.y = y;
-	this.p = p;
-	this.q = q;
-	this.g = g;
-}
+     @param y the public key
+     @param p the prime
+     @param q the sub-prime
+     @param g the base
+  */
+  public DSAPublicKeySpec(BigInteger x, BigInteger p, BigInteger q, BigInteger g) 
+  {
+    this.y = y;
+    this.p = p;
+    this.q = q;
+    this.g = g;
+  }
 
-/**
-	Returns public key y for the DSA algorithm.
+  /**
+     Returns public key y for the DSA algorithm.
 
-	@return Returns the requested BigInteger
-*/
-public BigInteger getY() 
-{
-	return this.y;
-}
+     @return Returns the requested BigInteger
+  */
+  public BigInteger getY() 
+  {
+    return this.y;
+  }
 
-/**
-	Returns p for the DSA algorithm.
+  /**
+     Returns p for the DSA algorithm.
 
-	@return Returns the requested BigInteger
-*/
-public BigInteger getP() 
-{
-	return this.q;
-}
+     @return Returns the requested BigInteger
+  */
+  public BigInteger getP() 
+  {
+    return this.q;
+  }
 
-/**
-	Returns p for the DSA algorithm.
+  /**
+     Returns p for the DSA algorithm.
 
-	@return Returns the requested BigInteger
-*/
-public BigInteger getQ() 
-{
-	return this.q;
-}
+     @return Returns the requested BigInteger
+  */
+  public BigInteger getQ() 
+  {
+    return this.q;
+  }
 
-/**
-	Returns g for the DSA algorithm.
+  /**
+     Returns g for the DSA algorithm.
 
-	@return Returns the requested BigInteger
-*/
-public BigInteger getG() 
-{
-	return this.g;
-}
+     @return Returns the requested BigInteger
+  */
+  public BigInteger getG() 
+  {
+    return this.g;
+  }
 
 }
--- java/security/spec/RSAKeyGenParameterSpec.java
+++ java/security/spec/RSAKeyGenParameterSpec.java
@@ -1,20 +1,29 @@
 /* RSAKeyGenParameterSpec.java --- RSA Key Generator Parameter Spec Class
-   
-  Copyright (c) 1999 by Free Software Foundation, Inc.
-  Written by Mark Benvenuto <mcb@gnu.org>
+   Copyright (C) 1999 Free Software Foundation, Inc.
 
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU Library General Public License as published 
-  by the Free Software Foundation, version 2. (see COPYING.LIB)
+This file is part of GNU Classpath.
 
-  This program is distributed in the hope that it will be useful, but
-  WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
+GNU Classpath is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+ 
+GNU Classpath is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
 
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software Foundation
-  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307 USA. */
+You should have received a copy of the GNU General Public License
+along with GNU Classpath; see the file COPYING.  If not, write to the
+Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+02111-1307 USA.
+
+As a special exception, if you link this library with other files to
+produce an executable, this library does not by itself cause the
+resulting executable to be covered by the GNU General Public License.
+This exception does not however invalidate any other reasons why the
+executable file might be covered by the GNU General Public License. */
+
 
 package java.security.spec;
 import java.math.BigInteger;
@@ -29,49 +38,49 @@ import java.math.BigInteger;
 */
 public class RSAKeyGenParameterSpec implements AlgorithmParameterSpec
 {
-    private int keysize;
-    private BigInteger publicExponent;
+  private int keysize;
+  private BigInteger publicExponent;
 
-    /**
-       Public Exponent F0 = 3
-     */
-    public static final BigInteger F0 = new BigInteger("3");
+  /**
+     Public Exponent F0 = 3
+  */
+  public static final BigInteger F0 = new BigInteger("3");
 
-    /**
-       Public Exponent F4 = 3
-     */
-    public static final BigInteger F4 = new BigInteger("65537");
+  /**
+     Public Exponent F4 = 3
+  */
+  public static final BigInteger F4 = new BigInteger("65537");
 
-    /**
-       Create a new RSAKeyGenParameterSpec to store the RSA key's keysize 
-       and public exponent
+  /**
+     Create a new RSAKeyGenParameterSpec to store the RSA key's keysize 
+     and public exponent
 
-       @param keysize Modulus size of key in bits
-       @param publicExponent - the exponent
-     */
-    public RSAKeyGenParameterSpec(int keysize, BigInteger publicExponent)
-    {
-	this.keysize = keysize;
-	this.publicExponent = publicExponent;
-    }
+     @param keysize Modulus size of key in bits
+     @param publicExponent - the exponent
+  */
+  public RSAKeyGenParameterSpec(int keysize, BigInteger publicExponent)
+  {
+    this.keysize = keysize;
+    this.publicExponent = publicExponent;
+  }
     
-    /**
-       Return the size of the key.
+  /**
+     Return the size of the key.
 
-       @return the size of the key.
-     */
-    public int getKeysize()
-    {
-	return keysize;
-    }
+     @return the size of the key.
+  */
+  public int getKeysize()
+  {
+    return keysize;
+  }
     
-    /**
-       Return the public exponent.
+  /**
+     Return the public exponent.
 
-       @return the public exponent.
-    */
-    public BigInteger getPublicExponent()
-    {
-	return publicExponent;
-    }
+     @return the public exponent.
+  */
+  public BigInteger getPublicExponent()
+  {
+    return publicExponent;
+  }
 }
