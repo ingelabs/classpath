1beda4960647595c4b90dbdbef916679e47b3ef4
Bryce McKinlay <mckinlay@redhat.com>
2000-07-12 04:56:52 +0000

    2000-07-12  Bryce McKinlay  <bryce@albatross.co.nz>
    	* java/applet/*.java: Reformatted to match current coding style.

--- java/applet/AudioClip.java
+++ java/applet/AudioClip.java
@@ -1,8 +1,8 @@
 /* AudioClip.java -- Play an audio clip.
    Copyright (C) 1999 Free Software Foundation, Inc.
-
+ 
 This file is part of GNU Classpath.
-
+ 
 GNU Classpath is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 2, or (at your option)
@@ -12,12 +12,12 @@ GNU Classpath is distributed in the hope that it will be useful, but
 WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 General Public License for more details.
-
+ 
 You should have received a copy of the GNU General Public License
 along with GNU Classpath; see the file COPYING.  If not, write to the
 Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
 02111-1307 USA.
-
+ 
 As a special exception, if you link this library with other files to
 produce an executable, this library does not by itself cause the
 resulting executable to be covered by the GNU General Public License.
@@ -34,29 +34,20 @@ package java.applet;
   */
 public interface AudioClip
 {
+  /**
+    * Plays the audio clip starting from the beginning.
+    */
+  void play();
 
-/**
-  * Plays the audio clip starting from the beginning.
-  */
-public abstract void
-play();
+  /**
+    * Stops playing this audio clip.  There is no mechanism for restarting
+    * at the point where the clip is stopped.
+    */
+  void stop();
 
-/*************************************************************************/
-
-/**
-  * Stops playing this audio clip.  There is no mechanism for restarting
-  * at the point where the clip is stopped.
-  */
-public abstract void
-stop();
-
-/*************************************************************************/
-
-/**
-  * Plays this audio clip in a continuous loop.
-  */
-public abstract void
-loop();
+  /**
+    * Plays this audio clip in a continuous loop.
+    */
+  void loop();
 
 } // interface AudioClip
-
