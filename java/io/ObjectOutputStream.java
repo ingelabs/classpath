af0dcf11d2e2afdd82d89d3af5e2f09d90d313a6
Warren Levy <warrenl@redhat.com>
2000-07-28 23:42:37 +0000

    * ObjectOutputStream.java (writeObject): Per spec, call 	NotSerializableException with just the class name.

--- java/io/ObjectOutputStream.java
+++ java/io/ObjectOutputStream.java
@@ -265,9 +265,7 @@ public class ObjectOutputStream extends OutputStream
 	Class clazz = obj.getClass ();
 	ObjectStreamClass osc = ObjectStreamClass.lookup (clazz);
 	if (osc == null)
-	  throw new NotSerializableException ("The class "
-					      + clazz.getName ()
-					      + " is not Serializable");
+	  throw new NotSerializableException (clazz.getName ());
 
 	if (clazz.isArray ())
 	{
@@ -332,9 +330,7 @@ public class ObjectOutputStream extends OutputStream
 	  break;
 	}
 
-	throw new NotSerializableException ("Instances of the class "
-					    + clazz.getName ()
-					    + " are not Serializable");
+	throw new NotSerializableException (clazz.getName ());
       } // end pseudo-loop
     }
     catch (IOException e)
