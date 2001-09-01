002405bffbaab757bc3a34c50919730f06dd7cd3
Tom Tromey <tromey@redhat.com>
2001-09-01 00:40:35 +0000

    * java/net/NetPermission.java: Re-merged with libgcj. 	* java/net/SocketPermission.java: Likewise.

--- java/net/NetPermission.java
+++ java/net/NetPermission.java
@@ -1,5 +1,5 @@
 /* NetPermission.java -- A class for basic miscellaneous network permission
-   Copyright (C) 1998,2000 Free Software Foundation, Inc.
+   Copyright (C) 1998, 2000 Free Software Foundation, Inc.
 
 This file is part of GNU Classpath.
 
@@ -7,7 +7,7 @@ GNU Classpath is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 2, or (at your option)
 any later version.
- 
+
 GNU Classpath is distributed in the hope that it will be useful, but
 WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
@@ -29,50 +29,37 @@ package java.net;
 import java.security.BasicPermission;
 
 /**
-  * This class is used to model miscellaneous network permissions.  It is
-  * a subclass of BasicPermission.  This means that it models a "boolean"
-  * permission.  One that you either have or do not have.  Thus there is
-  * no permitted action list associated with this object. 
-  *
-  * @author Aaron M. Renn (arenn@urbanophile.com)
-  */
-public final class NetPermission extends BasicPermission
-                                 implements java.io.Serializable
-{
-
-/*************************************************************************/
-
-/*
- * Constructors
+ * This class is used to model miscellaneous network permissions.  It is
+ * a subclass of BasicPermission.  This means that it models a "boolean"
+ * permission.  One that you either have or do not have.  Thus there is
+ * no permitted action list associated with this object. 
+ *
+ * @author Aaron M. Renn (arenn@urbanophile.com)
  */
-
-/**
-  * Initializes a new instance of <code>NetPermission</code> with the
-  * specified name.
-  *
-  * @param name The name of this permission.
-  */
-public
-NetPermission(String name)
+public final class NetPermission extends BasicPermission
+  implements java.io.Serializable
 {
-  super(name);
+  /**
+   * Initializes a new instance of <code>NetPermission</code> with the
+   * specified name.
+   *
+   * @param name The name of this permission.
+   */
+  public NetPermission(String name)
+  {
+    super(name);
+  }
+
+  /**
+   * Initializes a new instance of <code>NetPermission</code> with the 
+   * specified name and value.  Note that the value field is irrelevant and is 
+   * ignored.  This constructor should never need to be used.
+   *
+   * @param name The name of this permission
+   * @param perms The permitted actions of this permission (ignored)
+   */
+  public NetPermission(String name, String perms)
+  {
+    super(name);
+  }
 }
-
-/*************************************************************************/
-
-/**
-  * Initializes a new instance of <code>NetPermission</code> with the 
-  * specified name and value.  Note that the value field is irrelevant and is 
-  * ignored.  This constructor should never need to be used.
-  *
-  * @param name The name of this permission
-  * @param perms The permitted actions of this permission (ignored)
-  */
-public
-NetPermission(String name, String perms)
-{
-  super(name);
-}
-
-} // class NetPermission
-
