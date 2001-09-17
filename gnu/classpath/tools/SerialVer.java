a52b1137877942d7a77606191dcded8839ddf273
Brian Jones <cbj@gnu.org>
2001-09-17 03:50:35 +0000

    * gnu/classpath/tools/JavahMain.java: updated copyright/license * gnu/classpath/tools/Javap.java: updated copyright/license * gnu/classpath/tools/JavapMain.java: updated copyright/license * gnu/classpath/tools/SerialVer.java: updated copyright/license * gnu/classpath/tools/Util.java: updated copyright/license * gnu/classpath/tools/tester.j: updated copyright/license

--- gnu/classpath/tools/SerialVer.java
+++ gnu/classpath/tools/SerialVer.java
@@ -1,21 +1,21 @@
 /*************************************************************************
-/* Main.java -- Implementation of serialver program
-/*
-/* Copyright (c) 1998 by Free Software Foundation, Inc.
-/*
-/* This program is free software; you can redistribute it and/or modify
-/* it under the terms of the GNU Library General Public License as published 
-/* by the Free Software Foundation, version 2. (see COPYING.LIB)
-/*
-/* This program is distributed in the hope that it will be useful, but
-/* WITHOUT ANY WARRANTY; without even the implied warranty of
-/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-/* GNU General Public License for more details.
-/*
-/* You should have received a copy of the GNU General Public License
-/* along with this program; if not, write to the Free Software Foundation
-/* Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA
-/*************************************************************************/
+ * Main.java -- Implementation of serialver program
+ *
+ * Copyright (c) 2001 by Free Software Foundation, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published 
+ * by the Free Software Foundation, version 2. (see COPYING)
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation
+ * Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA
+ *************************************************************************/
 
 package gnu.classpath.tools;
 
