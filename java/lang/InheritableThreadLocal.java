9e8cc2c0609360efc5de0f79c860b98fa03b2c06
Mark Wielaard <mark@klomp.org>
2001-10-02 22:44:47 +0000

    Update copyright year

--- java/lang/InheritableThreadLocal.java
+++ java/lang/InheritableThreadLocal.java
@@ -1,5 +1,5 @@
 /* java.lang.InheritableThreadLocal
-   Copyright (C) 2000 Free Software Foundation, Inc.
+   Copyright (C) 2000, 2001 Free Software Foundation, Inc.
 
 This file is part of GNU Classpath.
 
