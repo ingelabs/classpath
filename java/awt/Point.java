b70a7472aa3bb8c5ad1d0e60c585a50c1fb302f9
Tom Tromey <tromey@redhat.com>
2002-01-16 21:35:37 +0000

    * java/awt/Point.java: Merged with libgcj.

--- java/awt/Point.java
+++ java/awt/Point.java
@@ -1,244 +1,150 @@
-/* Point.java -- Java representation of a point on a screen.
-   Copyright (C) 1999 Free Software Foundation, Inc.
+/* Copyright (C) 1999, 2002  Free Software Foundation
 
-This file is part of GNU Classpath.
+   This file is part of libjava.
 
-GNU Classpath is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2, or (at your option)
-any later version.
-
-GNU Classpath is distributed in the hope that it will be useful, but
-WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with GNU Classpath; see the file COPYING.  If not, write to the
-Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
-02111-1307 USA.
-
-As a special exception, if you link this library with other files to
-produce an executable, this library does not by itself cause the
-resulting executable to be covered by the GNU General Public License.
-This exception does not however invalidate any other reasons why the
-executable file might be covered by the GNU General Public License. */
-
+This software is copyrighted work licensed under the terms of the
+Libjava License.  Please consult the file "LIBJAVA_LICENSE" for
+details.  */
 
 package java.awt;
+import java.awt.geom.Point2D;
 
-/**
-  * This class represents a point on the screen using cartesian coordinates.
-  *
-  * @author Aaron M. Renn (arenn@urbanophile.com)
-  */
-public class Point implements java.io.Serializable
-{
-
-/*
- * Instance Variables
+/* Written using "Java Class Libraries", 2nd edition, plus online
+ * API docs for JDK 1.2 beta from http://www.javasoft.com.
+ * Status:  Believed complete and correct, except that neither toString
+ * nor hashCode have been compared with JDK output.
  */
 
 /**
-  * @serial The X coordinate of the point.
-  */
-public int x;
-
-/**
-  * @serial The Y coordinate of the point.
-  */
-public int y;
-
-/*************************************************************************/
-
-/*
- * Constructors
+ * This class represents a point on the screen using cartesian coordinates.
+ *
+ * @author Per Bothner <bothner@cygnus.com>
+ * @author Aaron M. Renn (arenn@urbanophile.com)
+ * @date February 8, 1999.
  */
-
-/**
-  * Initializes a new instance of <code>Point</code> representing the
-  * coordiates (0,0).
-  */
-public
-Point()
+public class Point extends Point2D implements java.io.Serializable
 {
-  this(0, 0);
-}
- 
-/*************************************************************************/
+  /**
+   * @serial The X coordinate of the point.
+   */
+  public int x;
 
-/**
-  * Initializes a new instance of <code>Point</code> with coordinates
-  * identical to the coordinates of the specified points.
-  *
-  * @param point The point to copy the coordinates from.
+  /**
+   * @serial The Y coordinate of the point.
+   */
+  public int y;
+
+  /**
+   * Initializes a new instance of <code>Point</code> representing the
+   * coordiates (0,0).
+   */
+  public Point () { }
+
+  /**
+   * Initializes a new instance of <code>Point</code> with coordinates
+   * identical to the coordinates of the specified points.
+   *
+   * @param point The point to copy the coordinates from.
+   */
+  public Point (Point p) { this.x = p.x;  this.y = p.y; }
+
+  /**
+   * Initializes a new instance of <code>Point</code> with the specified
+   * coordinates.
+   *
+   * @param x The X coordinate of this point.
+   * @param y The Y coordinate of this point.
+   */
+  public Point (int x, int y) { this.x = x;  this.y = y; }
+
+  /**
+   * Tests whether or not this object is equal to the specified object.
+   * This will be true if and only if the specified objectj:
+   * <p>
+   * <ul>
+   * <li>Is not <code>null</code>.
+   * <li>Is an instance of <code>Point</code>.
+   * <li>Has X and Y coordinates equal to this object's.
+   * </ul>
+   *
+   * @param obj The object to test against for equality.
+   *
+   * @return <code>true</code> if the specified object is equal to this
+   * object, <code>false</code> otherwise.
   */
-public
-Point(Point point)
-{
-  this(point.x, point.y);
+  public boolean equals (Object obj)
+  {
+    if (! (obj instanceof Point))
+      return false;
+    Point p = (Point) obj;
+    return this.x == p.x && this.y == p.y;
+  }
+
+  /**
+   * Returns a hash value for this point.
+   *
+   * @param A hash value for this point.
+   */
+  public int hashCode () { return x ^ y; }
+
+  /**
+   * Returns the location of this object as a point.  A pretty useless
+   * method.  It is included to mimic the <code>getLocation</code> method
+   * in component.
+   *
+   * @return This point.
+   */
+  public Point getLocation () { return new Point(this); }
+
+  /**
+   * Sets this object's coordinates to the specified values.  This method
+   * is identical to the <code>setLocation(int, int)</code> method.
+   *
+   * @param x The new X coordinate.
+   * @param y The new Y coordinate.
+   */
+  public void move (int x, int y) { this.x = x;  this.y = y; }
+
+  /**
+   * Sets this object's coordinates to the specified values.  This method
+   * is identical to the <code>move()</code> method.
+   *
+   * @param x The new X coordinate.
+   * @param y The new Y coordinate.
+   */
+  public void setLocation (int x, int y) { this.x = x;  this.y = y; }
+
+  /**
+   * Sets this object's coordinates to match those of the specified point.
+   *
+   * @param point The point to copy the coordinates from.
+   */
+  public void setLocation (Point pt) { this.x = pt.x;  this.y = pt.y; }
+
+  /**
+   * Changes the coordinates of this point such that the specified 
+   * <code>dx</code> parameter is added to the existing X coordinate and
+   * <code>dy</code> is added to the existing Y coordinate.
+   *
+   * @param dx The amount to add to the X coordinate.
+   * @param dy The amount to add to the Y coordinate.
+   */
+  public void translate (int x, int y) { this.x += x;  this.y += y; }
+
+  /**
+   * Returns a string representation of this object.
+   *
+   * @return A string representation of this object.
+   */
+  public String toString ()
+  {
+    return getClass().getName() + "[x:"+x+",y:"+y+']';
+  }
+
+  public double getX() { return x; }
+  public double getY() { return y; }
+
+  public void setLocation (double x, double y)
+  { this.x = (int) x;  this.y = (int) y; }
+
 }
-
-/*************************************************************************/
-
-/**
-  * Initializes a new instance of <code>Point</code> with the specified
-  * coordinates.
-  *
-  * @param x The X coordinate of this point.
-  * @param y The Y coordinate of this point.
-  */
-public 
-Point(int x, int y)
-{
-  this.x = x;
-  this.y = y;
-}
-
-/*************************************************************************/
-
-/*
- * Instance Methods
- */
-
-/**
-  * Returns the location of this object as a point.  A pretty useless
-  * method.  It is included to mimic the <code>getLocation</code> method
-  * in component.
-  *
-  * @return This point.
-  */
-public Point
-getLocation()
-{
-  return(this);
-}
-
-/*************************************************************************/
-
-/**
-  * Sets this object's coordinates to match those of the specified point.
-  *
-  * @param point The point to copy the coordinates from.
-  */
-public void
-setLocation(Point point)
-{
-  this.x = point.x;
-  this.y = point.y;
-}
-
-/*************************************************************************/
-
-/**
-  * Sets this object's coordinates to the specified values.  This method
-  * is identical to the <code>move()</code> method.
-  *
-  * @param x The new X coordinate.
-  * @param y The new Y coordinate.
-  */
-public void
-setLocation(int x, int y)
-{
-  this.x = x;
-  this.y = x;
-}
-
-/*************************************************************************/
-
-/**
-  * Sets this object's coordinates to the specified values.  This method
-  * is identical to the <code>setLocation(int, int)</code> method.
-  *
-  * @param x The new X coordinate.
-  * @param y The new Y coordinate.
-  */
-public void
-move(int x, int y)
-{
-  this.x = x;
-  this.y = y;
-}
-
-/*************************************************************************/
-
-/**
-  * Changes the coordinates of this point such that the specified 
-  * <code>dx</code> parameter is added to the existing X coordinate and
-  * <code>dy</code> is added to the existing Y coordinate.
-  *
-  * @param dx The amount to add to the X coordinate.
-  * @param dy The amount to add to the Y coordinate.
-  */
-public void
-translate(int dx, int dy)
-{
-  x += dx;
-  y += dy;
-}
-
-/*************************************************************************/
-
-/**
-  * Returns a hash value for this point.
-  *
-  * @param A hash value for this point.
-  */
-public int
-hashCode()
-{
-  return(((x*y)*(x*y)) % 37);
-}
-
-/*************************************************************************/
-
-/**
-  * Tests whether or not this object is equal to the specified object.
-  * This will be true if and only if the specified objectj:
-  * <p>
-  * <ul>
-  * <li>Is not <code>null</code>.
-  * <li>Is an instance of <code>Point</code>.
-  * <li>Has X and Y coordinates equal to this object's.
-  * </ul>
-  *
-  * @param obj The object to test against for equality.
-  *
-  * @return <code>true</code> if the specified object is equal to this
-  * object, <code>false</code> otherwise.
-  */
-public boolean
-equals(Object obj)
-{
-  if (obj == null)
-    return(false);
-
-  if (!(obj instanceof Point))
-    return(false);
-
-  Point p = (Point)obj;
-
-  if (p.x != x)
-    return(false);
-
-  if (p.y != y)
-    return(false);
-
-  return(true);
-}
-
-/*************************************************************************/
-
-/**
-  * Returns a string representation of this object.
-  *
-  * @return A string representation of this object.
-  */
-public String
-toString()
-{
-  return(getClass().getName() + ": (" + x + "," + y + ")");
-}
-
-} // class Point
-
