1ffcf18ed491cce2eb96ae953921af17b2f369d9
Tom Tromey <tromey@redhat.com>
2002-01-16 23:10:00 +0000

    * java/awt/Dimension.java: Merged with libgcj. 	* java/awt/Point.java: Updated copyright.

--- java/awt/Dimension.java
+++ java/awt/Dimension.java
@@ -1,5 +1,4 @@
-/* Dimension.java -- Class for width and height
-   Copyright (C) 1999 Free Software Foundation, Inc.
+/* Copyright (C) 1999, 2000, 2002  Free Software Foundation
 
 This file is part of GNU Classpath.
 
@@ -27,177 +26,136 @@ executable file might be covered by the GNU General Public License. */
 
 package java.awt;
 
+/* Written using "Java Class Libraries", 2nd edition, plus online
+ * API docs for JDK 1.2 beta from http://www.javasoft.com.
+ * Status:  Believed complete and correct, except that neither toString
+ * has not been compared with JDK output.
+ */
+
 /**
   * This class holds a width and height value pair.
   *
+  * @author Per Bothner <bothner@cygnus.com>
   * @author Aaron M. Renn (arenn@urbanophile.com)
+  * @date Fenruary 8, 1999.
   */
-public class Dimension implements java.io.Serializable
+public class Dimension extends java.awt.geom.Dimension2D
+  implements java.io.Serializable
 {
+  /**
+   * This width of this object.
+   */
+  public int width;
 
-/*
- * Instance Variables
- */
+  /**
+   * The height of this object.
+   */
+  public int height;
 
-/**
-  * This width of this object.
-  */
-public int width;
+  /**
+   * Initializes a new instance of <code>Dimension</code> with a width
+   * and height of zero.
+   */
+  public Dimension () { }
 
-/**
-  * The height of this object.
-  */
-public int height;
+  /**
+   * Initializes a new instance of <code>Dimension</code> to have a width
+   * and height identical to that of the specified dimension object.
+   *
+   * @param dim The <code>Dimension</code> to take the width and height from.
+   */
+  public Dimension (Dimension dim)
+  {
+    this.width = dim.width;
+    this.height = dim.height;
+  }
 
-/*************************************************************************/
+  /**
+   * Initializes a new instance of <code>Dimension</code> with the
+   * specified width and height.
+   *
+   * @param width The width of this object.
+   * @param height The height of this object.
+   */
+  public Dimension (int width, int height)
+  {
+    this.width = width;
+    this.height = height;
+  }
 
-/*
- * Constructors
- */
+  /**
+   * Tests this object for equality against the specified object.  This will
+   * be true if and only if the specified object:
+   * <p>
+   * <ul>
+   * <li>Is not <code>null</code>.
+   * <li>Is an instance of <code>Dimension</code>.
+   * <li>Has width and height values identical to this object.
+   * </ul>
+   *
+   * @param obj The object to test against.
+   *
+   * @return <code>true</code> if the specified object is equal to this
+   * object, <code>false</code> otherwise.
+   */
+  public boolean equals (Object obj)
+  {
+    if (! (obj instanceof Dimension))
+      return false;
+    Dimension dim = (Dimension) obj;
+    return height == dim.height && width == dim.width;
+  }
 
-/**
-  * Initializes a new instance of <code>Dimension</code> with a width
-  * and height of zero.
-  */
-public
-Dimension()
-{
+  /**
+   * Returns the size of this object.  Not very useful.
+   *
+   * @return This object.
+   */
+  public Dimension getSize () { return new Dimension(this); }
+
+  /**
+   * Sets the width and height of this object to match that of the
+   * specified object.
+   *
+   * @param dim The <code>Dimension</code> object to get the new width and
+   * height from.
+   */
+  public void setSize (Dimension dim)
+  {
+    this.width = dim.width;
+    this.height = dim.height;
+  }
+
+  /**
+   * Sets the width and height of this object to the specified values.
+   *
+   * @param width The new width value.
+   * @param height The new height value.
+   */
+  public void setSize (int width, int height)
+  {
+    this.width = width;
+    this.height = height;
+  }
+
+  /**
+   * Returns a string representation of this object.
+   *
+   * @return A string representation of this object.
+   */
+  public String toString ()
+  {
+    return "Dimension[w:"+width+",h:"+height+']';
+  }
+
+  /* Note:  There is no Dimension.hashCode. */
+
+  public double getWidth() { return width; }
+  public double getHeight() { return height; }
+
+  public void setSize (double width, double height)
+  {
+    this.width = (int) width;
+    this.height = (int) height;
+  }
 }
-
-/*************************************************************************/
-
-/**
-  * Initializes a new instance of <code>Dimension</code> to have a width
-  * and height identical to that of the specified dimension object.
-  *
-  * @param dim The <code>Dimension</code> to take the width and height from.
-  */
-public
-Dimension(Dimension dim)
-{
-  width = dim.width;
-  height = dim.height;
-
-  // Your a dim.width, Beavis!
-}
-
-/*************************************************************************/
-
-/**
-  * Initializes a new instance of <code>Dimension</code> with the
-  * specified width and height.
-  *
-  * @param width The width of this object.
-  * @param height The height of this object.
-  */
-public 
-Dimension(int width, int height)
-{
-  this.width = width;
-  this.height = height;
-}
-
-/*************************************************************************/
-
-/*
- * Instance Methods
- */
-
-/**
-  * Returns the size of this object.  Not very useful.
-  *
-  * @return This object.
-  */
-public Dimension
-getSize()
-{
-  return(this);
-}
-
-/*************************************************************************/
-
-/**
-  * Sets the width and height of this object to match that of the
-  * specified object.
-  *
-  * @param dim The <code>Dimension</code> object to get the new width and
-  * height from.
-  */
-public void
-setSize(Dimension dim)
-{
-  synchronized(this)
-    {
-      this.width = dim.width;
-      this.height = dim.height;
-    }
-}
-
-/*************************************************************************/
-
-/**
-  * Sets the width and height of this object to the specified values.
-  *
-  * @param width The new width value.
-  * @param height The new height value.
-  */
-public void
-setSize(int width, int height)
-{
-  synchronized(this)
-    {
-      this.width = width;
-      this.height = height;
-    }
-}
-
-/*************************************************************************/
-
-/**
-  * Tests this object for equality against the specified object.  This will
-  * be true if and only if the specified object:
-  * <p>
-  * <ul>
-  * <li>Is not <code>null</code>.
-  * <li>Is an instance of <code>Dimension</code>.
-  * <li>Has width and height values identical to this object.
-  * </ul>
-  *
-  * @param obj The object to test against.
-  *
-  * @return <code>true</code> if the specified object is equal to this
-  * object, <code>false</code> otherwise.
-  */
-public boolean
-equals(Object obj)
-{
-  if (obj == null)
-    return(false);
-
-  if (!(obj instanceof Dimension))
-    return(false);
-
-  Dimension dim = (Dimension)obj;
-
-  if ((dim.width != width) || (dim.height != height))
-    return(false);
-
-  return(true);
-}
-
-/*************************************************************************/
-
-/**
-  * Returns a string representation of this object.
-  *
-  * @return A string representation of this object.
-  */
-public String
-toString()
-{
-  return(getClass().getName() + "(width=" + width + ", height=" + height +
-         ")");
-}
-
-} // class Dimension 
-
