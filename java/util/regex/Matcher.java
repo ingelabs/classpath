0a84c39214e696bcb1d5b336d2a5c09fdb1389ba
John Leuner <jewel@pixie.co.za>
2002-03-08 20:44:52 +0000

    added stub classes for java.util.regex (new dependency from String)

---
+++ java/util/regex/Matcher.java
@@ -0,0 +1,6 @@
+package java.util.regex;
+
+public class Matcher
+{
+
+}
---
+++ java/util/regex/Pattern.java
@@ -0,0 +1,17 @@
+package java.util.regex;
+
+public class Pattern
+{
+    public static Pattern compile(String regex)
+    {
+	throw new IllegalArgumentException("Not implemented yet");
+    }
+    public static boolean matches(String regex, CharSequence input) 
+    {
+	throw new IllegalArgumentException("Not implemented yet");
+    }
+    public Matcher matcher(Pattern regex)
+    {
+	throw new IllegalArgumentException("Not implemented yet");
+    }
+}
