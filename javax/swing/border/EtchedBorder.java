643acbee260edbf5b483b3e5f727a430b2f616d2
Ronald Veldema <rveldema@cs.vu.nl>
2002-03-12 11:37:52 +0000

    *** empty log message ***

---
+++ javax/swing/border/EtchedBorder.java
@@ -0,0 +1,38 @@
+package javax.swing.border;
+
+import java.awt.*;
+
+public class EtchedBorder extends EmptyBorder
+{
+    Color c;
+
+    public EtchedBorder()
+    {
+    }
+
+    
+    public EtchedBorder(int top,
+		int left,
+		int bottom, 
+		int right,
+		Color color)
+    {
+        super(top, left, bottom, right);
+        this.c = color;
+    }
+
+    public boolean isBorderOpaque()
+    {
+	return false;
+    }
+    
+    public void paintBorder(Component  c,
+			    Graphics  g, 
+			    int  x, 
+			    int  y, 
+			    int  width, 
+			    int  height)
+    {
+    }
+}
+
