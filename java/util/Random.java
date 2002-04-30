d630c6754855f59d3b9512d8667a38747eacb247
Sascha Brawer <brawer@dandelis.ch>
2002-04-30 22:15:52 +0000

    setSeed, next, nextBytes, nextInt(), nextInt(int), nextLong, nextBoolean, nextFloat, nextDouble, nextGaussian: Docfix to avoid angle brackets in Javadoc and for pre-formatted code without asterisks.

--- java/util/Random.java
+++ java/util/Random.java
@@ -70,7 +70,7 @@ import java.io.Serializable;
  * @see java.security.SecureRandom
  * @see Math#random()
  * @author Jochen Hoenicke
- * @author Eric Blake <ebb9@email.byu.edu>
+ * @author Eric Blake (ebb9@email.byu.edu)
  * @status updated to 1.4
  */
 public class Random implements Serializable
@@ -139,12 +139,12 @@ public class Random implements Serializable
    * above, two instances of the same random class, starting with the
    * same seed, should produce the same results, if the same methods
    * are called.  The implementation for java.util.Random is:
-   * <pre>
-   * public synchronized void setSeed(long seed) {
-   *     this.seed = (seed ^ 0x5DEECE66DL) & ((1L << 48) - 1);
-   *     haveNextNextGaussian = false;
-   * }
-   * </pre>
+   *
+<pre>public synchronized void setSeed(long seed)
+{
+  this.seed = (seed ^ 0x5DEECE66DL) & ((1L &lt;&lt; 48) - 1);
+  haveNextNextGaussian = false;
+}</pre>
    *
    * @param seed the new seed
    */
@@ -159,12 +159,12 @@ public class Random implements Serializable
    * an int value whose <code>bits</code> low order bits are
    * independent chosen random bits (0 and 1 are equally likely).
    * The implementation for java.util.Random is:
-   * <pre>
-   * protected synchronized int next(int bits) {
-   *     seed = (seed * 0x5DEECE66DL + 0xBL) & ((1L << 48) - 1);
-   *     return (int) (seed >>> (48 - bits));
-   * }
-   * </pre>
+   *
+<pre>protected synchronized int next(int bits)
+{
+  seed = (seed * 0x5DEECE66DL + 0xBL) & ((1L &lt;&lt; 48) - 1);
+  return (int) (seed &gt;&gt;&gt; (48 - bits));
+}</pre>
    *
    * @param bits the number of random bits to generate, in the range 1..32
    * @return the next pseudorandom value
@@ -180,17 +180,19 @@ public class Random implements Serializable
    * Fills an array of bytes with random numbers.  All possible values
    * are (approximately) equally likely.
    * The JDK documentation gives no implementation, but it seems to be:
-   * <pre>
-   * public void nextBytes(byte[] bytes) {
-   *     for (int i=0; i< bytes.length; i+=4) {
-   *         int random = next(32);
-   *         for (int j=0; i+j< bytes.length && j<4; j++)
-   *             bytes[i+j] = (byte) (random & 0xff)
-   *             random >>= 8;
-   *         }
-   *     }
-   * }
-   * </pre>
+   *
+<pre>public void nextBytes(byte[] bytes)
+{
+  for (int i = 0; i &lt; bytes.length; i += 4)
+  {
+    int random = next(32);
+    for (int j = 0; i + j &lt; bytes.length && j &lt; 4; j++)
+    {
+      bytes[i+j] = (byte) (random & 0xff)
+      random &gt;&gt;= 8;
+    }
+  }
+}</pre>
    *
    * @param bytes the byte array that should be filled
    * @throws NullPointerException if bytes is null
@@ -225,11 +227,11 @@ public class Random implements Serializable
    * an int value whose 32 bits are independent chosen random bits
    * (0 and 1 are equally likely).  The implementation for
    * java.util.Random is:
-   * <pre>
-   * public int nextInt() {
-   *     return next(32);
-   * }
-   * </pre>
+   * 
+<pre>public int nextInt()
+{
+  return next(32);
+}</pre>
    *
    * @return the next pseudorandom value
    */
@@ -244,21 +246,28 @@ public class Random implements Serializable
    * each value has the same likelihodd (1/<code>n</code>).
    * (0 and 1 are equally likely).  The implementation for
    * java.util.Random is:
-   * <pre>
-   * public int nextInt(int n) {
-   *     if (n<=0)
-   *         throw new IllegalArgumentException("n must be positive");
-   *     if ((n & -n) == n)  // i.e., n is a power of 2
-   *         return (int)((n * (long)next(31)) >> 31);
-   *     int bits, val;
-   *     do {
-   *         bits = next(32);
-   *         val = bits % n;
-   *     } while(bits - val + (n-1) < 0);
-   *     return val;
-   * }
-   * </pre>
-   * This algorithm would return every value with exactly the same
+   * 
+<pre>
+public int nextInt(int n)
+{
+  if (n &lt;= 0)
+    throw new IllegalArgumentException("n must be positive");
+
+  if ((n & -n) == n)  // i.e., n is a power of 2
+    return (int)((n * (long) next(31)) &gt;&gt; 31);
+
+  int bits, val;
+  do
+  {
+    bits = next(32);
+    val = bits % n;
+  }
+  while(bits - val + (n-1) &lt; 0);
+
+  return val;
+}</pre>
+   *   
+   * <p>This algorithm would return every value with exactly the same
    * probability, if the next()-method would be a perfect random number
    * generator.
    *
@@ -298,11 +307,11 @@ public class Random implements Serializable
    * Generates the next pseudorandom long number.  All bits of this
    * long are independently chosen and 0 and 1 have equal likelihood.
    * The implementation for java.util.Random is:
-   * <pre>
-   * public long nextLong() {
-   *     return ((long)next(32) << 32) + next(32);
-   * }
-   * </pre>
+   *
+<pre>public long nextLong()
+{
+  return ((long) next(32) &lt;&lt; 32) + next(32);
+}</pre>
    *
    * @return the next pseudorandom value
    */
@@ -314,11 +323,11 @@ public class Random implements Serializable
   /**
    * Generates the next pseudorandom boolean.  True and false have
    * the same probability.  The implementation is:
-   * <pre>
-   * public boolean nextBoolean() {
-   *     return next(1) != 0;
-   * }
-   * </pre>
+   * 
+<pre>public boolean nextBoolean()
+{
+  return next(1) != 0;
+}</pre>
    *
    * @return the next pseudorandom boolean
    * @since 1.2
@@ -332,11 +341,11 @@ public class Random implements Serializable
    * Generates the next pseudorandom float uniformly distributed
    * between 0.0f (inclusive) and 1.0f (exclusive).  The
    * implementation is as follows.
-   * <pre>
-   * public float nextFloat() {
-   *     return next(24) / ((float)(1 << 24));
-   * }
-   * </pre>
+   * 
+<pre>public float nextFloat()
+{
+  return next(24) / ((float)(1 &lt;&lt; 24));
+}</pre>
    *
    * @return the next pseudorandom float
    */
@@ -349,11 +358,11 @@ public class Random implements Serializable
    * Generates the next pseudorandom double uniformly distributed
    * between 0.0 (inclusive) and 1.0 (exclusive).  The
    * implementation is as follows.
-   * <pre>
-   * public double nextDouble() {
-   *     return (((long)next(26) << 27) + next(27)) / (double)(1L << 53);
-   * }
-   * </pre>
+   *
+<pre>public double nextDouble()
+{
+  return (((long) next(26) &lt;&lt; 27) + next(27)) / (double)(1L &lt;&lt; 53);
+}</pre>
    *
    * @return the next pseudorandom double
    */
@@ -366,26 +375,33 @@ public class Random implements Serializable
    * Generates the next pseudorandom, Gaussian (normally) distributed
    * double value, with mean 0.0 and standard deviation 1.0.
    * The algorithm is as follows.
-   * <pre>
-   * public synchronized double nextGaussian() {
-   *     if (haveNextNextGaussian) {
-   *         haveNextNextGaussian = false;
-   *         return nextNextGaussian;
-   *     } else {
-   *         double v1, v2, s;
-   *         do {
-   *             v1 = 2 * nextDouble() - 1; // between -1.0 and 1.0
-   *             v2 = 2 * nextDouble() - 1; // between -1.0 and 1.0
-   *             s = v1 * v1 + v2 * v2;
-   *         } while (s >= 1);
-   *         double norm = Math.sqrt(-2 * Math.log(s)/s);
-   *         nextNextGaussian = v2 * norm;
-   *         haveNextNextGaussian = true;
-   *         return v1 * norm;
-   *     }
-   * }
-   * </pre>
-   * This is described in section 3.4.1 of <em>The Art of Computer
+   * 
+<pre>public synchronized double nextGaussian()
+{
+  if (haveNextNextGaussian)
+  {
+    haveNextNextGaussian = false;
+    return nextNextGaussian;
+  }
+  else
+  {
+    double v1, v2, s;
+    do
+    {
+      v1 = 2 * nextDouble() - 1; // between -1.0 and 1.0
+      v2 = 2 * nextDouble() - 1; // between -1.0 and 1.0
+      s = v1 * v1 + v2 * v2;
+    }
+    while (s >= 1);
+
+    double norm = Math.sqrt(-2 * Math.log(s) / s);
+    nextNextGaussian = v2 * norm;
+    haveNextNextGaussian = true;
+    return v1 * norm;
+  }
+}</pre>
+   *
+   * <p>This is described in section 3.4.1 of <em>The Art of Computer
    * Programming, Volume 2</em> by Donald Knuth.
    *
    * @return the next pseudorandom Gaussian distributed double
