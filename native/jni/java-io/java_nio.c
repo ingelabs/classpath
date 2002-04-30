0af9cde0e0018ed2b5e51c86296a4821c18a29d4
Etienne M. Gagnon <etienne.gagnon@uqam.ca>
2002-04-30 02:38:58 +0000

    Solved compile time problems under gcc 2.95.x and added missing Copyright statement.

--- native/jni/java-io/java_nio.c
+++ native/jni/java-io/java_nio.c
@@ -1,3 +1,40 @@
+/* java_nio.c - Native methods for java.io.File class
+   Copyright (C) 2002 Free Software Foundation, Inc.
+
+This file is part of GNU Classpath.
+
+GNU Classpath is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+ 
+GNU Classpath is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU Classpath; see the file COPYING.  If not, write to the
+Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+02111-1307 USA.
+
+Linking this library statically or dynamically with other modules is
+making a combined work based on this library.  Thus, the terms and
+conditions of the GNU General Public License cover the whole
+combination.
+
+As a special exception, the copyright holders of this library give you
+permission to link this library with independent modules to produce an
+executable, regardless of the license terms of these independent
+modules, and to copy and distribute the resulting executable under
+terms of your choice, provided that you also meet, for each linked
+independent module, the terms and conditions of the license of that
+module.  An independent module is a module which is not derived from
+or based on this library.  If you modify this library, you may extend
+this exception to your version of the library, but you are not
+obligated to do so.  If you do not wish to do so, delete this
+exception statement from your version. */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -59,8 +96,9 @@ jlong convert_Long(jlong X)
     (((int)a[1]) << 16) + 			
     (((int)a[2]) << 8) + 			
     (((int)a[3]) << 0);		
+  int res2;
   a += 4;
-  int res2 =					
+  res2 =					
     (((int)a[0]) << 24) + 			
     (((int)a[1]) << 16) + 			
     (((int)a[2]) << 8) + 			
@@ -252,7 +290,7 @@ void nio_unmmap_file(int fd,
 }
 
 
-u_int64_t Java_gnu_java_nio_FileChannelImpl_nio_1mmap_1file(JNIEnv *env,
+jlong Java_gnu_java_nio_FileChannelImpl_nio_1mmap_1file(JNIEnv *env,
 							    jclass c,
 							    jint fd,
 							    jlong pos,
