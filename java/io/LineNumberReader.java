a0bd56d892116938a17ba8aa5c4eabc2c190194b
Aaron M. Renn <arenn@urbanophile.com>
2003-03-10 00:03:45 +0000

    * java/io/LineNumberReader.java         Merge with gcj by replacing all methods with the versions from         gcj with the exception of skip() This file is in essence         completely new except for the Javadoc comments, which are carried         over from Classpath

--- java/io/LineNumberReader.java
+++ java/io/LineNumberReader.java
@@ -1,5 +1,5 @@
 /* LineNumberReader.java -- A character input stream which counts line numbers
-   Copyright (C) 1998,2003 Free Software Foundation, Inc.
+   Copyright (C) 1998, 2003 Free Software Foundation, Inc.
 
 This file is part of GNU Classpath.
 
@@ -7,7 +7,7 @@ GNU Classpath is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 2, or (at your option)
 any later version.
- 
+
 GNU Classpath is distributed in the hope that it will be useful, but
 WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
@@ -35,72 +35,54 @@ this exception to your version of the library, but you are not
 obligated to do so.  If you do not wish to do so, delete this
 exception statement from your version. */
 
-
 package java.io;
 
 /**
-  * This class functions like a standard <code>Reader</code> except that it
-  * counts line numbers, and canonicalizes newline characters.  As data
-  * is read, whenever the char sequences "\r", "\n", or "\r\n" are encountered,
-  * the running line count is incremeted by one.  Additionally, the whatever
-  * line termination sequence was encountered will be converted to a "\n"
-  * char.  Note that this class numbers lines from 0.  When the first
-  * line terminator is encountered, the line number is incremented to 1, and
-  * so on.  Also note that actual "\r" and "\n" characters are looked for.
-  * The system dependent line separator sequence is ignored.
-  * <p>
-  * This class counts only line termination characters.  If the last line
-  * read from the stream does not end in a line termination sequence, it
-  * will not be counted as a line.
-  *
-  * @author Aaron M. Renn (arenn@urbanophile.com)
-  */
+ * This class functions like a standard <code>Reader</code> except that it
+ * counts line numbers, and canonicalizes newline characters.  As data
+ * is read, whenever the char sequences "\r", "\n", or "\r\n" are encountered,
+ * the running line count is incremeted by one.  Additionally, the whatever
+ * line termination sequence was encountered will be converted to a "\n"
+ * char.  Note that this class numbers lines from 0.  When the first
+ * line terminator is encountered, the line number is incremented to 1, and
+ * so on.  Also note that actual "\r" and "\n" characters are looked for.
+ * The system dependent line separator sequence is ignored.
+ * <p>
+ * This class counts only line termination characters.  If the last line
+ * read from the stream does not end in a line termination sequence, it
+ * will not be counted as a line.
+ *
+ * @author Per Bothner <bothner@cygnus.com>
+ * @author Aaron M. Renn (arenn@urbanophile.com)
+ * @date April 22, 1998.  
+ */
+/* Written using "Java Class Libraries", 2nd edition, plus online
+ * API docs for JDK 1.2 beta from http://www.javasoft.com.
+ * Status:  Believed complete and correct.
+ *
+ * This implementation has the feature that if '\r' is read, it
+ * does not look for a '\n', but immediately returns '\n'.
+ * On the next read(), if a '\n' is read, it is skipped.
+ * This has the advantage that we do not read (and hang) unnecessarily.
+ *
+ * This implementation is also minimal in the number of fields it uses.
+ */
 public class LineNumberReader extends BufferedReader
 {
-
-  /*************************************************************************/
-
-  /*
-   * Instance Variables
-   */
+  /** The current line number. */
+  private int lineNumber;
 
   /**
-    * This variable is used to keep track of the current line number
-    */
-  private int line_number;
-
-  /**
-    * This variable is used to keep track of the line number that was
-    * current when the <code>mark()</code> method was called.
-    */
-  private int marked_line_number;
-
-  /**
-    * Determines whether or not a '\n' as the first character of a read
-    * is the continuation of a '\r' as the last character of the previous
-    * read or not
-    */
-  private boolean continued_newline;
-
-  /*************************************************************************/
-
-  /*
-   * Constructors
-   */
-
-  /**
-    * Create a new <code>LineNumberReader</code> that reads from the 
-    * specified subordinate <code>Reader</code>.  A default 4096 char sized
+    * Create a new <code>LineNumberReader</code> that reads from the
+    * specified subordinate <code>Reader</code>.  A default 8K char sized
     * buffer will be used for reads.
     *
     * @param in The subordinate <code>Reader</code> to read from
     */
   public LineNumberReader(Reader in)
   {
-    this(in, DEFAULT_BUFFER_SIZE);
+    super(in, DEFAULT_BUFFER_SIZE);
   }
-
-  /*************************************************************************/
 
   /**
     * This method initializes a new <code>LineNumberReader</code> to read
@@ -115,12 +97,6 @@ public class LineNumberReader extends BufferedReader
     super(in, size);
   }
 
-  /*************************************************************************/
-
-  /*
-   * Instance Methods
-   */
-
   /**
     * This method returns the current line number
     *
@@ -128,85 +104,105 @@ public class LineNumberReader extends BufferedReader
     */
   public int getLineNumber()
   {
-    return(line_number);
+    return lineNumber;
   }
-
-  /*************************************************************************/
 
   /**
     * This method sets the current line number to the specified value.
-    * 
+    *
     * @param line_number The new line number
     */
-  public void setLineNumber(int line_number)
+  public void setLineNumber(int lineNumber)
   {
-    this.line_number = line_number;
+    this.lineNumber = lineNumber;
   }
 
-  /*************************************************************************/
+  private static int countLines (char[] buffer, int off, int len)
+  {
+    int count = 0;
+    char prev = '\0';
+    for (int i = 0;  i < len;  i++)
+      {
+        char ch = buffer[i+off];
+        if ((ch == '\n' && prev != '\r') || ch == '\r')
+          count++;
+        prev = ch;
+      }
+    return count;
+  }
 
   /**
     * This method marks a position in the input to which the stream can be
     * "reset" char calling the <code>reset()</code> method.  The parameter
     * <code>readlimit</code> is the number of chars that can be read from the
     * stream after setting the mark before the mark becomes invalid.   For
-    * example, if <code>mark()</code> is called with a read limit of 10, 
+    * example, if <code>mark()</code> is called with a read limit of 10,
     * then when
     * 11 chars of data are read from the stream before the <code>reset()</code>
     * method is called, then the mark is invalid and the stream object
     * instance is not required to remember the mark.
     * <p>
     * In this class, this method will remember the current line number as well
-    * as the current position in the stream.  When the <code>reset()</code> 
-    * method 
+    * as the current position in the stream.  When the <code>reset()</code>
+    * method
     * is called, the line number will be restored to the saved line number in
     * addition to the stream position.
     *
-    * @param readlimit The number of chars that can be read before the 
+    * @param readlimit The number of chars that can be read before the
     * mark becomes invalid
     *
     * @exception IOException If an error occurs
     */
-  public void mark(int readlimit) throws IOException
+  public void mark(int readLimit) throws IOException
   {
-    synchronized (lock) {
-
-    super.mark(readlimit);
-
-    marked_line_number = line_number;
-
-    } // synchronized
+    synchronized (lock)
+      {
+	// This is basically the same as BufferedReader.mark.
+	// However, if the previous character was a '\r', we need to
+	// save that 'r', in case the next character is a '\n'.
+	if (pos + readLimit > limit)
+	  {
+	    int saveCR = (pos > 0 && buffer[pos-1] == '\r') ? 1 : 0;
+	    char[] old_buffer = buffer;
+	    if (readLimit > limit)
+	      buffer = new char[saveCR + readLimit];
+	    int copy_start = pos - saveCR;
+	    limit -= copy_start;
+	    System.arraycopy(old_buffer, copy_start, buffer, 0, limit);
+	    pos = saveCR;
+	  }
+	markPos = pos;
+      }
   }
 
-  /*************************************************************************/
-
   /**
-    * This method resets a stream to the point where the <code>mark()</code> 
+    * This method resets a stream to the point where the <code>mark()</code>
     * method
     * was called.  Any chars that were read after the mark point was set will
     * be re-read during subsequent reads.
     * <p>
     * In this class, this method will also restore the line number that was
     * current when the <code>mark()</code> method was called.
-    * 
+    *
     * @exception IOException If an error occurs
     */
   public void reset() throws IOException
   {
-    synchronized (lock) {
-
-    super.reset();
-
-    line_number = marked_line_number;
-
-    } // synchronized
+    synchronized (lock)
+      {
+	if (markPos < 0)
+	  throw new IOException("mark never set or invalidated");
+	if (markPos > 0 && pos > markPos && buffer[markPos-1] == '\r'
+	    && buffer[markPos] == '\n')
+	  lineNumber--;
+	lineNumber -= countLines(buffer, markPos, pos - markPos);
+	pos = markPos;
+      }
   }
-
-  /*************************************************************************/
 
   /**
     * This method reads an unsigned char from the input stream and returns it
-    * as an int in the range of 0-255.  This method will return -1 if the
+    * as an int in the range of 0-65535.  This method will return -1 if the
     * end of the stream has been reached.
     * <p>
     * Note that if a line termination sequence is encountered (ie, "\r",
@@ -219,39 +215,38 @@ public class LineNumberReader extends BufferedReader
     * to be read.
     *
     * @return The char read or -1 if end of stream
-    * 
+    *
     * @exception IOException If an error occurs
     */
   public int read() throws IOException
   {
-    synchronized (lock) 
+    synchronized (lock)
       {
-        int char_read = super.read();
-
-        if (char_read == '\n')
-          ++line_number;
-
-        if (char_read == '\r')
-          {
-            int extra_char_read = super.read();
-
-            if ((extra_char_read != '\n') && (extra_char_read != -1))
-              pos--;
-
-            char_read = '\n';
-            ++line_number;
-          }
-
-        return(char_read);
-      } // synchronized
+	skipRedundantLF();
+	if (pos >= limit)
+	  {
+	    if (markPos >= 0 && limit == buffer.length)
+	      markPos = -1;
+	    if (markPos <= 0)
+	      pos = limit = 0;
+	    int count = in.read(buffer, limit, buffer.length - limit);
+	    if (count <= 0)
+	      return -1;
+	    limit += count;
+	  }
+	char ch = buffer[pos++];
+	if (ch == '\r' || ch == '\n')
+	  {
+	    lineNumber++;
+	    return '\n';
+	  }
+	return (int) ch;
+      }
   }
-
-  /*************************************************************************/
 
   /**
     * This method reads chars from a stream and stores them into a caller
-    * supplied buffer.  It starts storing data at index <code>offset</code> into
-    * the buffer and attemps to read <code>len</code> chars.  This method can
+    * supplied buffer.  It starts storing data at index <code>offset</code> into    * the buffer and attemps to read <code>len</code> chars.  This method can
     * return before reading the number of chars requested.  The actual number
     * of chars read is returned as an int.  A -1 is returned to indicated the
     * end of the stream.
@@ -271,48 +266,64 @@ public class LineNumberReader extends BufferedReader
     *
     * @exception IOException If an error occurs.
     */
-  public int read(char[] buf, int offset, int len) throws IOException
+  public int read(char[] buf, int offset, int count) throws IOException
   {
-    if (len == 0)
-      return(0);
-
-    synchronized (lock) 
+    if (count <= 0)
       {
-
-        // Read the first char here in order to allow IOException's to 
-        // propagate up
-        int char_read = read();
-        if (char_read == -1)
-          return(-1);
-        buf[offset] = (char)char_read;
-
-        int total_read = 1;
-
-        // Read the rest of the chars.  We do this in a single char read() loop,
-        // which is bad and should be fixed in the future. *****RECODE THIS****
-        try
-          {
-            for (int i = 1; i < len; i++)
-              {
-                char_read = read();
-                if (char_read == -1)
-                  return(total_read);
-
-                buf[offset + i] = (char)char_read;
-                
-                ++total_read;  
-              }
-          }
-        catch (IOException e)
-          {
-            return(total_read);
-          }
-
-        return(total_read);
-      } // synchronized
+	if (count < 0)
+	  throw new IndexOutOfBoundsException();
+	return 0;
+      }
+    synchronized (lock)
+      {
+	int first = read();
+	if (first < 0)
+	  return -1;
+	int start_offset = offset;
+	buf[offset++] = (char) first;
+	if (buffer[pos-1] == '\r' && pos < limit && buffer[pos] == '\n')
+	  pos++;
+	count--;
+	while (count-- > 0 && pos < limit)
+	  {
+	    char ch = buffer[pos++];
+	    if (ch == '\r')
+	      {
+		lineNumber++;
+		ch = '\n';
+		if (pos < limit && buffer[pos] == '\n')
+		  pos++;
+	      }
+	    else if (ch == '\n')
+	      lineNumber++;
+	    buf[offset++] = ch;
+	  }
+	return offset - start_offset;
+      }
   }
 
-  /*************************************************************************/
+  private void skipRedundantLF() throws IOException
+  {
+    if (pos > 0 && buffer[pos-1] == '\r')
+      {
+	if (pos < limit)
+	  { // fast case
+	    if (buffer[pos] == '\n')
+	      pos++;
+	  }
+	else
+	  { // use read() to deal with the general case.
+	    // Set pos and limit to zero to avoid infinite recursion in read.
+	    // May need to invalidate markPos if we've exceeded the buffer.  
+	    if (pos >= buffer.length)
+	      markPos = -1;
+	    pos = limit = 0;
+	    int ch = read();
+	    if (ch >= 0 && ch != '\n')
+	      pos--;
+	  }
+      }
+  }
 
   /**
     * This method reads a line of text from the input stream and returns
@@ -320,24 +331,36 @@ public class LineNumberReader extends BufferedReader
     * by a "\r", "\n", or "\r\n" sequence, not by the system dependent line
     * separator.
     *
-    * @return The line read as a <code>String</code> or <code>null</code> 
+    * @return The line read as a <code>String</code> or <code>null</code>
     * if end of stream.
     *
     * @exception IOException If an error occurs
     */
   public String readLine() throws IOException
   {
-    synchronized (lock) 
-      {
-        String line = super.readLine();
-        if (line != null)
-          ++line_number;
+    // BufferedReader.readLine already does this.  Shouldn't need to keep
+    // track of newlines (since the read method deals with this for us).
+    // But if the buffer is large, we may not call the read method at all
+    // and super.readLine can't increment lineNumber itself.
+    // Though it may seem kludgy, the safest thing to do is to save off
+    // lineNumber and increment it explicitly when we're done (iff we
+    // ended with a '\n' or '\r' as opposed to EOF).
+    //
+    // Also, we need to undo the special casing done by BufferedReader.readLine
+    // when a '\r' is the last char in the buffer.  That situation is marked
+    // by 'pos > limit'.
+    int tmpLineNumber = lineNumber;
+    skipRedundantLF();
+    String str = super.readLine();
+    if (pos > limit)
+      --pos;
 
-        return(line);
-      } // synchronized
+    int ch;
+    if (pos > 0 && ((ch = buffer[pos - 1]) == '\n' || ch == '\r'))
+      lineNumber = tmpLineNumber + 1;
+
+    return str;
   }
-
-  /*************************************************************************/
 
   /**
     * This method skips over characters in the stream.  This method will
@@ -345,18 +368,51 @@ public class LineNumberReader extends BufferedReader
     * to skip them all.  The actual number of characters skipped is returned.
     * This method returns 0 if the specified number of chars is less than 1.
     *
-    * @param num_chars The specified number of chars to skip.
+    * @param count The specified number of chars to skip.
     *
-    * @return The actual number of chars skipped. 
+    * @return The actual number of chars skipped.
     *
     * @exception IOException If an error occurs
     */
-  public long skip(long num_chars) throws IOException
+  public long skip(long count) throws IOException
   {
-    // The spec says to override this method so we do even though we
-    // don't need to.  Assume we aren't counting lines in skipped chars.
-    return(super.skip(num_chars));
+    return(super.skip(count)); 
+    // FIXME: This method not merged from gcj. The code below is the original
+    // gcj code that doesn't work.  I'm not sure if the method above (from
+    // Classpath) is broken or not, but it seems to work correctly in all
+    // methods I've tested.  If somebody wants to investigate why the code
+    // below fails, feel free.
+/*
+    if (count <= 0)
+      return 0;
+    long to_do = count;
+    do
+      {
+	int ch = read();
+	if (ch < 0)
+	  break;
+	to_do--;
+	if (ch == '\n' || ch == '\r')
+	  lineNumber++;
+	else
+	  {
+	    long fence = pos + to_do;
+	    if (limit < fence)
+	      fence = limit;
+	    int end = pos;
+	    for (; end < fence; end++)
+	      {
+		char endch = buffer[end];
+		if (endch == '\n' || endch == '\r')
+		  break;
+	      }
+	    to_do -= end - pos;
+	    pos = end;
+	  }
+      }
+    while (to_do > 0);
+    return count - to_do;
+*/
   }
-
-} // class LineNumberReader
+}
 
