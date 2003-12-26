21e79f918d831d6fcd235afedefc95025dafcc5d
Michael Koch <konqueror@gmx.de>
2003-12-26 22:21:56 +0000

    2003-12-26  Michael Koch  <konqueror@gmx.de>
    	* java/net/DatagramSocket.java
    	(close): Directly return if socket is closed.
    	* java/net/ServerSocket.java bind():
    	If InetSocketAddress.getAddress() returns "null" use "0.0.0.0" as
    	address to bind to.
    	(close): Directly return if socket is closed.

--- java/net/ServerSocket.java
+++ java/net/ServerSocket.java
@@ -221,14 +221,20 @@ public class ServerSocket
       throw new IllegalArgumentException ("Address type not supported");
 
     InetSocketAddress tmp = (InetSocketAddress) endpoint;
-    
+
     SecurityManager s = System.getSecurityManager ();
     if (s != null)
       s.checkListen (tmp.getPort ());
 
+    InetAddress addr = tmp.getAddress();
+    
+    // Initialize addr with 0.0.0.0.
+    if (addr == null)
+      addr = InetAddress.ANY_IF;
+    
     try
       {
-	impl.bind (tmp.getAddress (), tmp.getPort ());
+	impl.bind(addr, tmp.getPort());
 	impl.listen(backlog);
 	bound = true;
       }
@@ -353,15 +359,15 @@ public class ServerSocket
    */
   public void close () throws IOException
   {
-    if (!isClosed())
-      {
-	impl.close();
-	impl = null;
-	bound = false;
+    if (isClosed())
+      return;
+    
+    impl.close();
+    impl = null;
+    bound = false;
 
-	if (getChannel() != null)
-	  getChannel().close();
-      }
+    if (getChannel() != null)
+      getChannel().close();
   }
 
   /**
