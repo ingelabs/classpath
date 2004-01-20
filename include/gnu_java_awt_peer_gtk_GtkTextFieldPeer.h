77825cd451e5189a11007660c870f3794b441bfa
Thomas Fitzsimmons <fitzsim@redhat.com>
2004-01-20 21:33:35 +0000

    2004-01-20  Thomas Fitzsimmons  <fitzsim@redhat.com>
    	* include/gnu_java_awt_peer_gtk_GtkTextFieldPeer.h: Regenerate.

--- include/gnu_java_awt_peer_gtk_GtkTextFieldPeer.h
+++ include/gnu_java_awt_peer_gtk_GtkTextFieldPeer.h
@@ -10,8 +10,8 @@ extern "C"
 {
 #endif
 
-extern JNIEXPORT void JNICALL Java_gnu_java_awt_peer_gtk_GtkTextFieldPeer_create (JNIEnv *env, jobject);
-extern JNIEXPORT void JNICALL Java_gnu_java_awt_peer_gtk_GtkTextFieldPeer_gtkEntryGetSize (JNIEnv *env, jobject, jintArray);
+extern JNIEXPORT void JNICALL Java_gnu_java_awt_peer_gtk_GtkTextFieldPeer_create__I (JNIEnv *env, jobject, jint);
+extern JNIEXPORT jint JNICALL Java_gnu_java_awt_peer_gtk_GtkTextFieldPeer_gtkEntryGetBorderWidth (JNIEnv *env, jobject);
 extern JNIEXPORT void JNICALL Java_gnu_java_awt_peer_gtk_GtkTextFieldPeer_gtkSetFont (JNIEnv *env, jobject, jstring, jint, jint);
 extern JNIEXPORT void JNICALL Java_gnu_java_awt_peer_gtk_GtkTextFieldPeer_setEchoChar (JNIEnv *env, jobject, jchar);
 
