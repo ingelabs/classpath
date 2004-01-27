878f618dea5737916799b66a6d8d5cf1f2e16230
Michael Koch <konqueror@gmx.de>
2004-01-27 22:35:55 +0000

    2004-01-27  Michael Koch  <konqueror@gmx.de>
    	* gnu/java/awt/peer/gtk/GtkFileDialogPeer.java: Merged copyright year.

--- gnu/java/awt/peer/gtk/GtkFileDialogPeer.java
+++ gnu/java/awt/peer/gtk/GtkFileDialogPeer.java
@@ -1,5 +1,5 @@
 /* GtkFileDialogPeer.java -- Implements FileDialogPeer with GTK
-   Copyright (C) 1998, 1999, 2002 Free Software Foundation, Inc.
+   Copyright (C) 1998, 1999, 2002, 2004 Free Software Foundation, Inc.
 
 This file is part of GNU Classpath.
 
