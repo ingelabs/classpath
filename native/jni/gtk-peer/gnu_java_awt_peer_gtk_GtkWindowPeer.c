9295045ac131c00bb98e0ca89a11f1be65843962
Mark Wielaard <mark@klomp.org>
2004-03-12 21:40:59 +0000

    2004-03-12  Thomas Fitzsimmons  <fitzsim@redhat.com>
            * native/jni/gtk-peer/gnu_java_awt_peer_gtk_GtkWindowPeer.c
            (connectJObject): Replace printf calls with g_assert statements.
            Move property-notify-event signal connection to ...
            (connectSignals): Connect property-notify-event signal.  Iterate
            through the vbox's children to find layout.

--- native/jni/gtk-peer/gnu_java_awt_peer_gtk_GtkWindowPeer.c
+++ native/jni/gtk-peer/gnu_java_awt_peer_gtk_GtkWindowPeer.c
@@ -188,11 +188,8 @@ Java_gnu_java_awt_peer_gtk_GtkWindowPeer_connectJObject
 
   children = gtk_container_get_children(GTK_CONTAINER(ptr));
   vbox = children->data;
+  g_assert (GTK_IS_VBOX(vbox));
 
-  if(!GTK_IS_VBOX(vbox))
-    {
-      printf("*** this is not a vbox\n");
-    }
   children = gtk_container_get_children(GTK_CONTAINER(vbox));
   do
   {
@@ -200,11 +197,7 @@ Java_gnu_java_awt_peer_gtk_GtkWindowPeer_connectJObject
     children = children->next;
   }
   while (!GTK_IS_LAYOUT (layout) && children != NULL);
-
-  if(!GTK_IS_LAYOUT(layout))
-    {
-      printf("*** widget is not a layout ***");
-    }
+  g_assert (GTK_IS_LAYOUT(layout));
 
   gtk_widget_realize (layout);
 
@@ -213,9 +206,6 @@ Java_gnu_java_awt_peer_gtk_GtkWindowPeer_connectJObject
   gtk_widget_realize (ptr);
 
   connect_awt_hook (env, obj, 1, GTK_WIDGET (ptr)->window);
-
-  g_signal_connect (G_OBJECT (ptr), "property-notify-event",
-		    G_CALLBACK (window_property_changed_cb), obj);
 
   gdk_threads_leave ();
 }
@@ -237,10 +227,16 @@ Java_gnu_java_awt_peer_gtk_GtkWindowPeer_connectSignals
   /* Receive events from the GtkLayout too */
   children = gtk_container_get_children(GTK_CONTAINER(ptr));
   vbox = children->data;  
-  g_assert(GTK_IS_VBOX(vbox));
+  g_assert (GTK_IS_VBOX (vbox));
+
   children = gtk_container_get_children(GTK_CONTAINER(vbox));
-  layout = children->data;  
-  g_assert(GTK_IS_LAYOUT(layout));
+  do
+  {
+    layout = children->data;
+    children = children->next;
+  }
+  while (!GTK_IS_LAYOUT (layout) && children != NULL);
+  g_assert (GTK_IS_LAYOUT (layout));
 
   g_signal_connect (GTK_OBJECT (layout), "event", 
 		    G_CALLBACK (pre_event_handler), *gref);
@@ -263,6 +259,9 @@ Java_gnu_java_awt_peer_gtk_GtkWindowPeer_connectSignals
 
   g_signal_connect (G_OBJECT (ptr), "window-state-event",
 		    G_CALLBACK (window_window_state_cb), *gref);
+
+  g_signal_connect (G_OBJECT (ptr), "property-notify-event",
+		    G_CALLBACK (window_property_changed_cb), *gref);
 
   gdk_threads_leave ();
 
