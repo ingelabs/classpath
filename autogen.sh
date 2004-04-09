9d68675206a6cd1e986add4de790c4359c9db820
Mark Wielaard <mark@klomp.org>
2004-04-09 17:14:44 +0000

    * autogen.sh: Check for explicit versions of the autotools installed        in /usr/bin.

--- autogen.sh
+++ autogen.sh
@@ -2,26 +2,51 @@
 
 set -e
 
-libtoolize --version | head -1
+LIBTOOLIZE=libtoolize
+${LIBTOOLIZE} --version | head -1
 echo "libtoolize: minimum version required: 1.4.2"
-autoconf --version | head -1
+
+AUTOCONF=autoconf
+${AUTOCONF} --version | head -1
 echo "autoconf: minimum version required: 2.59"
-automake --version | head -1
+
+# Autoheader is part of autoconf
+AUTOHEADER=autoheader
+${AUTOHEADER} --version | head -1
+echo "autoheader: minimum version required: 2.59"
+
+AUTOMAKE=automake
+if test -x /usr/bin/automake-1.8; then
+  AUTOMAKE=/usr/bin/automake-1.8
+elif test -x /usr/bin/automake-1.7; then
+  AUTOMAKE=/usr/bin/automake-1.7
+fi
+${AUTOMAKE} --version | head -1
 echo "automake: minimum version required: 1.7.0"
 
+# Aclocal is part of automake
+ACLOCAL=aclocal
+if test -x /usr/bin/aclocal-1.8; then
+  ACLOCAL=/usr/bin/aclocal-1.8
+elif test -x /usr/bin/aclocal-1.7; then
+  ACLOCAL=/usr/bin/aclocal-1.7
+fi
+${ACLOCAL} --version | head -1
+echo "aclocal: minimum version required: 1.7.0"
+
 echo "libtoolize ..."
-libtoolize --force --copy
+${LIBTOOLIZE} --force --copy
 
 echo "aclocal ..."
-aclocal
+${ACLOCAL}
 
 echo "autoheader ..."
-autoheader --force
+${AUTOHEADER} --force
 
 echo "automake ..."
-automake --add-missing --copy
+${AUTOMAKE} --add-missing --copy
 
 echo "autoconf ..."
-autoconf --force
+${AUTOCONF} --force
 
 echo "Finished"
