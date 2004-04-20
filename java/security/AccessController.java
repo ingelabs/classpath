568ab40fe90dcf11d2250ca274253ac5b87c17bc
Michael Koch <konqueror@gmx.de>
2004-04-20 19:18:15 +0000

    2004-04-20  Michael Koch  <konqueror@gmx.de>
    	* java/security/AccessController.java:
    	Merged coypright year from libgcj.

--- java/security/AccessController.java
+++ java/security/AccessController.java
@@ -1,5 +1,5 @@
 /* AccessController.java --- Access control context and permission checker
-   Copyright (C) 2001 Free Software Foundation, Inc.
+   Copyright (C) 2001, 2004  Free Software Foundation, Inc.
 
 This file is part of GNU Classpath.
 
