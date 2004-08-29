2ada683534541853c247b0138000b8b1259418fb
Michael Koch <konqueror@gmx.de>
2004-08-29 16:32:13 +0000

    2004-08-29  Michael Koch  <konqueror@gmx.de>
    	* configure.ac: Depend on automake 1.9.
    	* INSTALL: Likewise.
    	* INSTALL: Updated to depend on automake 1.9 and libtool 1.5.
    	* autogen.sh: Likewise.
    	* configure.ac (AM_INIT_AUTOMAKE): Depend on automake 1.9, support
    	long paths in dist tarball.

--- autogen.sh
+++ autogen.sh
@@ -4,7 +4,7 @@ set -e
 
 LIBTOOLIZE=libtoolize
 ${LIBTOOLIZE} --version | head -n 1
-echo "libtoolize: minimum version required: 1.4.2"
+echo "libtoolize: minimum version required: 1.5"
 
 AUTOCONF=autoconf
 ${AUTOCONF} --version | head -n 1
@@ -16,23 +16,19 @@ ${AUTOHEADER} --version | head -n 1
 echo "autoheader: minimum version required: 2.59"
 
 AUTOMAKE=automake
-if test -x /usr/bin/automake-1.8; then
-  AUTOMAKE=/usr/bin/automake-1.8
-elif test -x /usr/bin/automake-1.7; then
-  AUTOMAKE=/usr/bin/automake-1.7
+if test -x /usr/bin/automake-1.9; then
+  AUTOMAKE=/usr/bin/automake-1.9
 fi
 ${AUTOMAKE} --version | head -n 1
-echo "automake: minimum version required: 1.7.0"
+echo "automake: minimum version required: 1.9.0"
 
 # Aclocal is part of automake
 ACLOCAL=aclocal
-if test -x /usr/bin/aclocal-1.8; then
-  ACLOCAL=/usr/bin/aclocal-1.8
-elif test -x /usr/bin/aclocal-1.7; then
-  ACLOCAL=/usr/bin/aclocal-1.7
+if test -x /usr/bin/aclocal-1.9; then
+  ACLOCAL=/usr/bin/aclocal-1.9
 fi
 ${ACLOCAL} --version | head -n 1
-echo "aclocal: minimum version required: 1.7.0"
+echo "aclocal: minimum version required: 1.9.0"
 
 echo "libtoolize ..."
 ${LIBTOOLIZE} --force --copy
