6dde77342eed7f11c3fd466149b935cc27e8b343
Michael Koch <konqueror@gmx.de>
2004-10-15 07:27:14 +0000

    2004-10-15  Michael Koch  <konqueror@gmx.de>
    	* java/nio/ByteOrder.java,
    	java/nio/MappedByteBuffer.java,
    	java/nio/MappedByteBufferImpl.java,
    	java/nio/channels/Channels.java,
    	java/nio/channels/spi/SelectorProvider.java,
    	java/nio/charset/Charset.java:
    	Cleanup.

--- java/nio/charset/Charset.java
+++ java/nio/charset/Charset.java
@@ -35,7 +35,10 @@ this exception to your version of the library, but you are not
 obligated to do so.  If you do not wish to do so, delete this
 exception statement from your version. */
 
+
 package java.nio.charset;
+
+import gnu.java.nio.charset.Provider;
 
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
@@ -47,7 +50,6 @@ import java.util.Locale;
 import java.util.Set;
 import java.util.SortedMap;
 import java.util.TreeMap;
-import gnu.java.nio.charset.Provider;
 
 /**
  * @author Jesse Rosenstock
