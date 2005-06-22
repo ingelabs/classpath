71b6026396f2caad67d7d253777e84c6d33640db
Mark Wielaard <mark@klomp.org>
2005-06-22 23:31:47 +0000

    * gnu/java/awt/peer/gtk/GdkPixbufDecoder.java         (pumpBytes): Throws IOException.         (pumpDone): New native method.         (produce): Call pumpDone().         * include/gnu_java_awt_peer_gtk_GdkPixbufDecoder.h: Regenerated.         * native/jni/gtk-peer/gnu_java_awt_peer_gtk_GdkPixbufDecoder.c         (Java_gnu_java_awt_peer_gtk_GdkPixbufDecoder_pumpDone): New method.         (Java_gnu_java_awt_peer_gtk_GdkPixbufDecoder_pumpBytes): Check for         errors on gdk_pixbuf_loader_write.

--- include/gnu_java_awt_peer_gtk_GdkPixbufDecoder.h
+++ include/gnu_java_awt_peer_gtk_GdkPixbufDecoder.h
@@ -13,6 +13,7 @@ extern "C"
 JNIEXPORT void JNICALL Java_gnu_java_awt_peer_gtk_GdkPixbufDecoder_initStaticState (JNIEnv *env, jclass);
 JNIEXPORT void JNICALL Java_gnu_java_awt_peer_gtk_GdkPixbufDecoder_initState (JNIEnv *env, jobject);
 JNIEXPORT void JNICALL Java_gnu_java_awt_peer_gtk_GdkPixbufDecoder_pumpBytes (JNIEnv *env, jobject, jbyteArray, jint);
+JNIEXPORT void JNICALL Java_gnu_java_awt_peer_gtk_GdkPixbufDecoder_pumpDone (JNIEnv *env, jobject);
 JNIEXPORT void JNICALL Java_gnu_java_awt_peer_gtk_GdkPixbufDecoder_finish (JNIEnv *env, jobject);
 JNIEXPORT void JNICALL Java_gnu_java_awt_peer_gtk_GdkPixbufDecoder_streamImage (JNIEnv *env, jclass, jintArray, jstring, jint, jint, jboolean, jobject);
 
