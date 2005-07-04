6eaa0a1a249a0317fc7ff2db5c88805bb7ac1947
Thomas Fitzsimmons <fitzsim@redhat.com>
2005-07-04 23:56:35 +0000

    2005-07-04  Thomas Fitzsimmons  <fitzsim@redhat.com>
    	* gnu/java/awt/peer/gtk/GtkComponentPeer.java (postTextEvent): New
    	method.
    	* gnu/java/awt/peer/gtk/GtkTextAreaPeer.java: Add
    	TextComponentPeer method declarations.
    	* gnu/java/awt/peer/gtk/GtkTextFieldPeer.java: Likewise.
    	* gnu/java/awt/peer/gtk/GtkTextComponentPeer.java: Remove file.
    	* include/gnu_java_awt_peer_gtk_GtkTextAreaPeer.h: Regenerate.
    	* include/gnu_java_awt_peer_gtk_GtkTextFieldPeer.h: Likewise.
    	* native/jni/gtk-peer/Makefile.am (libgtkpeer_la_SOURCES): Remove
    	gnu_java_awt_peer_gtk_GtkTextComponentPeer.c.
    	* native/jni/gtk-peer/gnu_java_awt_peer_gtk_GtkComponentPeer.c
    	(classpath_gtk_component_connect_nonfocus_signals): New function.
    	(classpath_gtk_component_connect_signals): Likewise.
    	* native/jni/gtk-peer/gnu_java_awt_peer_gtk_GtkTextAreaPeer.c: Add
    	method implementations from
    	gnu_java_awt_peer_gtk_GtkTextComponentPeer.c.
    	* native/jni/gtk-peer/gnu_java_awt_peer_gtk_GtkTextFieldPeer.c:
    	Likewise.
    	* native/jni/gtk-peer/gnu_java_awt_peer_gtk_GtkTextComponentPeer.c:
    	Remove file.
    	* native/jni/gtk-peer/gnu_java_awt_peer_gtk_GtkToolkit.c
    	(gtkInit): Get postTextEventID from GtkComponentPeer, not
    	GtkTextComponentPeer.
    	* native/jni/gtk-peer/gtkpeer.h (classpath_gtk_textcomponent_init_jni): Declare.
    	(classpath_gtk_component_connect_signals): Likewise.
    	(classpath_gtk_component_connect_nonfocus_signals): Likewise.
    	(classpath_gtk_textcomponent_connect_signals): Likewise.

--- include/gnu_java_awt_peer_gtk_GtkTextFieldPeer.h
+++ include/gnu_java_awt_peer_gtk_GtkTextFieldPeer.h
@@ -13,6 +13,15 @@ extern "C"
 JNIEXPORT void JNICALL Java_gnu_java_awt_peer_gtk_GtkTextFieldPeer_create (JNIEnv *env, jobject, jint);
 JNIEXPORT void JNICALL Java_gnu_java_awt_peer_gtk_GtkTextFieldPeer_gtkWidgetSetBackground (JNIEnv *env, jobject, jint, jint, jint);
 JNIEXPORT void JNICALL Java_gnu_java_awt_peer_gtk_GtkTextFieldPeer_gtkWidgetSetForeground (JNIEnv *env, jobject, jint, jint, jint);
+JNIEXPORT void JNICALL Java_gnu_java_awt_peer_gtk_GtkTextFieldPeer_connectSignals (JNIEnv *env, jobject);
+JNIEXPORT jint JNICALL Java_gnu_java_awt_peer_gtk_GtkTextFieldPeer_getCaretPosition (JNIEnv *env, jobject);
+JNIEXPORT void JNICALL Java_gnu_java_awt_peer_gtk_GtkTextFieldPeer_setCaretPosition (JNIEnv *env, jobject, jint);
+JNIEXPORT jint JNICALL Java_gnu_java_awt_peer_gtk_GtkTextFieldPeer_getSelectionStart (JNIEnv *env, jobject);
+JNIEXPORT jint JNICALL Java_gnu_java_awt_peer_gtk_GtkTextFieldPeer_getSelectionEnd (JNIEnv *env, jobject);
+JNIEXPORT jstring JNICALL Java_gnu_java_awt_peer_gtk_GtkTextFieldPeer_getText (JNIEnv *env, jobject);
+JNIEXPORT void JNICALL Java_gnu_java_awt_peer_gtk_GtkTextFieldPeer_select (JNIEnv *env, jobject, jint, jint);
+JNIEXPORT void JNICALL Java_gnu_java_awt_peer_gtk_GtkTextFieldPeer_setEditable (JNIEnv *env, jobject, jboolean);
+JNIEXPORT void JNICALL Java_gnu_java_awt_peer_gtk_GtkTextFieldPeer_setText (JNIEnv *env, jobject, jstring);
 JNIEXPORT jint JNICALL Java_gnu_java_awt_peer_gtk_GtkTextFieldPeer_gtkEntryGetBorderWidth (JNIEnv *env, jobject);
 JNIEXPORT void JNICALL Java_gnu_java_awt_peer_gtk_GtkTextFieldPeer_gtkWidgetModifyFont (JNIEnv *env, jobject, jstring, jint, jint);
 JNIEXPORT void JNICALL Java_gnu_java_awt_peer_gtk_GtkTextFieldPeer_setEchoChar (JNIEnv *env, jobject, jchar);
