e5d84bbc425bfa27c02d2ea711edecf39694ef10
Roman Kennke <roman@kennke.org>
2005-07-22 12:34:35 +0000

    2005-07-22  Roman Kennke  <roman@kennke.org>
    	* javax/swing/JToggleButton.java
    	(ToggleButtonModel.setPressed): Fixed order in which events
    	get fired.

--- javax/swing/JToggleButton.java
+++ javax/swing/JToggleButton.java
@@ -145,7 +145,20 @@ public class JToggleButton extends AbstractButton implements Accessible
       // if this call does not represent a CHANGE in state, then return
       if ((p && isPressed()) || (!p && !isPressed()))
         return;
-      
+
+      // The JDK first fires events in the following order:
+      // 1. ChangeEvent for selected
+      // 2. ChangeEvent for pressed
+      // 3. ActionEvent
+      // So do we.
+
+      // setPressed(false) == mouse release on us,
+      // if we were armed, we flip the selected state.
+      if (!p && isArmed())
+        {
+          setSelected(! isSelected());
+        }
+
       // make the change
       if (p)
         stateMask = stateMask | PRESSED;
@@ -154,16 +167,14 @@ public class JToggleButton extends AbstractButton implements Accessible
       
       // notify interested ChangeListeners
       fireStateChanged();
-      
-      // setPressed(false) == mouse release on us,
-      // if we were armed, we flip the selected state.
+
       if (!p && isArmed())
         {
           fireActionPerformed(new ActionEvent(this,
                                               ActionEvent.ACTION_PERFORMED,
                                               actionCommand));
-          setSelected(! isSelected());
         }
+
     }
   }
 
