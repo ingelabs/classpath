941d46f5b4bd1b4380fcf5ad6754de509001769f
Roman Kennke <roman@kennke.org>
2005-07-29 23:02:01 +0000

    2005-07-29  Roman Kennke  <roman@kennke.org>
    	* javax/swing/text/StyledEditorKit.java: Added the remaining API
    	comments.

--- javax/swing/text/StyledEditorKit.java
+++ javax/swing/text/StyledEditorKit.java
@@ -455,15 +455,29 @@ public class StyledEditorKit extends DefaultEditorKit
   }
 
   /**
-   * StyledViewFactory
+   * A {@link ViewFactory} that is able to create {@link View}s for
+   * the <code>Element</code>s that are supported by
+   * <code>StyledEditorKit</code>, namely the following types of Elements:
+   *
+   * <ul>
+   * <li>{@link AbstractDocument.ContentElementName}</li>
+   * <li>{@link AbstractDocument.ParagraphElementName}</li>
+   * <li>{@link AbstractDocument.SectionElementName}</li>
+   * <li>{@link StyleContext.ComponentElementName}</li>
+   * <li>{@link StyleContext.IconElementName}</li>
+   * </ul>
    */
   static class StyledViewFactory
     implements ViewFactory
   {
     /**
-     * create
-     * @param value0 TODO
-     * @returns View
+     * Creates a {@link View} for the specified <code>Element</code>.
+     *
+     * @param element the <code>Element</code> to create a <code>View</code>
+     *        for
+     * @return the <code>View</code> for the specified <code>Element</code>
+     *         or <code>null</code> if the type of <code>element</code> is
+     *         not supported
      */
     public View create(Element element)
     {
@@ -485,14 +499,16 @@ public class StyledEditorKit extends DefaultEditorKit
   }
 
   /**
-   * AttributeTracker
+   * Keeps track of the caret position and updates the currentRun
+   * <code>Element</code> and the <code>inputAttributes</code>.
    */
   class CaretTracker
     implements CaretListener
   {
     /**
-     * caretUpdate
-     * @param value0 TODO
+     * Notifies an update of the caret position.
+     *
+     * @param ev the event for the caret update
      */
     public void caretUpdate(CaretEvent ev)
     {
@@ -514,12 +530,13 @@ public class StyledEditorKit extends DefaultEditorKit
   }
 
   /**
-   * currentRun
+   * Stores the <code>Element</code> at the current caret position. This
+   * is updated by {@link CaretTracker}.
    */
   Element currentRun;
 
   /**
-   * inputAttributes
+   * The current input attributes. This is updated by {@link CaretTracker}.
    */
   MutableAttributeSet inputAttributes;
 
@@ -535,7 +552,7 @@ public class StyledEditorKit extends DefaultEditorKit
   StyledViewFactory viewFactory;
 
   /**
-   * Constructor StyledEditorKit
+   * Creates a new instance of <code>StyledEditorKit</code>.
    */
   public StyledEditorKit()
   {
@@ -543,8 +560,9 @@ public class StyledEditorKit extends DefaultEditorKit
   }
 
   /**
-   * clone
-   * @returns Object
+   * Creates an exact copy of this <code>StyledEditorKit</code>.
+   *
+   * @return an exact copy of this <code>StyledEditorKit</code>
    */
   public Object clone()
   {
@@ -554,8 +572,16 @@ public class StyledEditorKit extends DefaultEditorKit
   }
 
   /**
-   * getActions
-   * @returns Action[]
+   * Returns the <code>Action</code>s supported by this {@link EditorKit}.
+   * This includes the {@link BoldAction}, {@link ItalicAction} and
+   * {@link UnderlineAction} as well as the <code>Action</code>s supported
+   * by {@link DefaultEditorKit}.
+   *
+   * The other <code>Action</code>s of <code>StyledEditorKit</code> are not
+   * returned here, since they require a parameter and thus custom
+   * instantiation.
+   *
+   * @return the <code>Action</code>s supported by this {@link EditorKit}
    */
   public Action[] getActions()
   {
@@ -566,8 +592,10 @@ public class StyledEditorKit extends DefaultEditorKit
   }
 
   /**
-   * getInputAttributes
-   * @returns MutableAttributeSet
+   * Returns the current input attributes. These are automatically set on
+   * any newly inserted content, if not specified otherwise.
+   *
+   * @return the current input attributes
    */
   public MutableAttributeSet getInputAttributes()
   {
@@ -575,8 +603,11 @@ public class StyledEditorKit extends DefaultEditorKit
   }
 
   /**
-   * getCharacterAttributeRun
-   * @returns Element
+   * Returns the {@link Element} that represents the character run at the
+   * current caret position.
+   *
+   * @return the {@link Element} that represents the character run at the
+   *         current caret position
    */
   public Element getCharacterAttributeRun()
   {
@@ -584,8 +615,12 @@ public class StyledEditorKit extends DefaultEditorKit
   }
 
   /**
-   * createDefaultDocument
-   * @returns Document
+   * Creates the default {@link Document} supported by this
+   * <code>EditorKit</code>. This is an instance of
+   * {@link DefaultStyledDocument} in this case but may be overridden by
+   * subclasses.
+   *
+   * @return an instance of <code>DefaultStyledDocument</code>
    */
   public Document createDefaultDocument()
   {
@@ -593,8 +628,12 @@ public class StyledEditorKit extends DefaultEditorKit
   }
 
   /**
-   * install
-   * @param component TODO
+   * Installs this <code>EditorKit</code> on the specified {@link JEditorPane}.
+   * This basically involves setting up required listeners on the
+   * <code>JEditorPane</code>.
+   *
+   * @param component the <code>JEditorPane</code> to install this
+   *        <code>EditorKit</code> on
    */
   public void install(JEditorPane component)
   {
@@ -603,8 +642,13 @@ public class StyledEditorKit extends DefaultEditorKit
   }
 
   /**
-   * deinstall
-   * @param component TODO
+   * Deinstalls this <code>EditorKit</code> from the specified
+   * {@link JEditorPane}. This basically involves removing all listeners from
+   * <code>JEditorPane</code> that have been set up by this
+   * <code>EditorKit</code>.
+   *
+   * @param component the <code>JEditorPane</code> from which to deinstall this
+   *        <code>EditorKit</code>
    */
   public void deinstall(JEditorPane component)
   {
@@ -615,8 +659,20 @@ public class StyledEditorKit extends DefaultEditorKit
   }
 
   /**
-   * getViewFactory
-   * @returns ViewFactory
+   * Returns a {@link ViewFactory} that is able to create {@link View}s
+   * for {@link Element}s that are supported by this <code>EditorKit</code>,
+   * namely the following types of <code>Element</code>s:
+   *
+   * <ul>
+   * <li>{@link AbstractDocument.ContentElementName}</li>
+   * <li>{@link AbstractDocument.ParagraphElementName}</li>
+   * <li>{@link AbstractDocument.SectionElementName}</li>
+   * <li>{@link StyleContext.ComponentElementName}</li>
+   * <li>{@link StyleContext.IconElementName}</li>
+   * </ul>
+   *
+   * @return a {@link ViewFactory} that is able to create {@link View}s
+   *          for {@link Element}s that are supported by this <code>EditorKit</code>
    */
   public ViewFactory getViewFactory()
   {
@@ -626,9 +682,17 @@ public class StyledEditorKit extends DefaultEditorKit
   }
 
   /**
-   * createInputAttributes
-   * @param element TODO
-   * @param set TODO
+   * Copies the text attributes from <code>element</code> to <code>set</code>.
+   * This is called everytime when the caret position changes to keep
+   * track of the current input attributes. The attributes in <code>set</code>
+   * are cleaned before adding the attributes of <code>element</code>.
+   *
+   * This method filters out attributes for element names, <code>Icon</code>s
+   * and <code>Component</code>s.
+   *
+   * @param element the <code>Element</code> from which to copy the text
+   *         attributes
+   * @param set the inputAttributes to copy the attributes to
    */
   protected void createInputAttributes(Element element,
 				       MutableAttributeSet set)
