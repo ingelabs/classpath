e1cbdc847cdad456b82862487307e7def807d99a
Roman Kennke <roman@kennke.org>
2005-07-29 10:47:14 +0000

    2005-07-29  Roman Kennke  <roman@kennke.org>
    	* javax/swing/text/AbstractDocument.java
    	(createPosition): Delegate this call to the actual Content.
    	(LeafElement.constructor): Manage the start and end marks via
    	Position objects.
    	(LeafElement.getEndOffset): Use Position instead of static mark.
    	(LeafElement.getStartOffset): Use Position instead of static mark.
    	* javax/swing/text/DefaultStyledDocument.java
    	(ElementBuffer.change): New method. Performs structural changes
    	in the element tree that are necessary in order to change
    	text attributes.
    	(ElementBuffer.changeUpdate): New method. This is the method for
    	change().
    	(ElementBuffer.split): New package-private method. Splits an
    	element into two elements.
    	(setCharacterAttributes): Implemented this method. This sets
    	character attributes on a piece of content.
    	* javax/swing/text/GapContent.java
    	(GapContentPosition): New inner class. Implements the Position
    	interface for GapContent.
    	(constructor): Initialize the list that stores the Position objects.
    	(createPosition): Reimplemented. Now uses the GapContentPosition
    	class.
    	(shiftEnd): Update the stored positions.
    	(shiftGap): Update the stored positions.
    	* javax/swing/text/LabelView.java: New class.

--- javax/swing/text/DefaultStyledDocument.java
+++ javax/swing/text/DefaultStyledDocument.java
@@ -42,6 +42,8 @@ import java.awt.Color;
 import java.awt.Font;
 import java.io.Serializable;
 
+import javax.swing.event.DocumentEvent;
+
 /**
  * @author Michael Koch (konqueror@gmx.de)
  */
@@ -52,7 +54,13 @@ public class DefaultStyledDocument extends AbstractDocument
     implements Serializable
   {
     private Element root;
-    
+
+    /** Holds the offset for structural changes. */
+    private int offset;
+
+    /** Holds the length of structural changes. */
+    private int length;
+
     public ElementBuffer(Element root)
     {
       this.root = root;
@@ -61,6 +69,82 @@ public class DefaultStyledDocument extends AbstractDocument
     public Element getRootElement()
     {
       return root;
+    }
+
+    /**
+     * Modifies the element structure so that the specified interval starts
+     * and ends at an element boundary. Content and paragraph elements
+     * are split and created as necessary.
+     *
+     * This also updates the <code>DefaultDocumentEvent</code> to reflect the
+     * structural changes.
+     *
+     * The bulk work is delegated to {@link #changeUpdate()}.
+     *
+     * @param offset the start index of the interval to be changed
+     * @param length the length of the interval to be changed
+     * @param ev the <code>DefaultDocumentEvent</code> describing the change
+     */
+    public void change(int offset, int length, DefaultDocumentEvent ev)
+    {
+      this.offset = offset;
+      this.length = length;
+      changeUpdate();
+    }
+
+    /**
+     * Performs the actual work for {@link #change}.
+     * The elements at the interval boundaries are split up (if necessary)
+     * so that the interval boundaries are located at element boundaries.
+     */
+    protected void changeUpdate()
+    {
+      // Split up the element at the start offset if necessary.
+      Element el = getCharacterElement(offset);
+      split(el, offset);
+
+      int endOffset = offset + length;
+      el = getCharacterElement(endOffset);
+      split(el, endOffset);
+    }
+
+    /**
+     * Splits an element if <code>offset</code> is not alread at its boundary.
+     *
+     * @param el the Element to possibly split
+     * @param offset the offset at which to possibly split
+     */
+    void split(Element el, int offset)
+    {
+      if (el instanceof AbstractElement)
+	{
+	  AbstractElement ael = (AbstractElement) el;
+	  int startOffset = ael.getStartOffset();
+	  int endOffset = ael.getEndOffset();
+	  int len = endOffset - startOffset;
+	  if (startOffset != offset && endOffset != offset)
+	    {
+	      Element paragraph = ael.getParentElement();
+	      if (paragraph instanceof BranchElement)
+		{
+		  BranchElement par = (BranchElement) paragraph;
+		  Element child1 = createLeafElement(par, ael, startOffset,
+						     offset);
+		  Element child2 = createLeafElement(par, ael, offset,
+						     endOffset);
+		  int index = par.getElementIndex(startOffset);
+		  par.replace(index, 1, new Element[]{ child1, child2 });
+		}
+	    }
+	  else
+	    throw new AssertionError("paragraph elements are expected to be "
+				     + "instances of "
+			  + "javax.swing.text.AbstractDocument.BranchElement");
+	}
+      else
+	throw new AssertionError("content elements are expected to be "
+				 + "instances of "
+			+ "javax.swing.text.AbstractDocument.AbstractElement");
     }
   }
   
@@ -175,8 +259,48 @@ public class DefaultStyledDocument extends AbstractDocument
 				     AttributeSet attributes,
 				     boolean replace)
   {
-    // FIXME: Implement me.
-    throw new Error("not implemented");
+    DefaultDocumentEvent ev =
+      new DefaultDocumentEvent(offset, length,
+			       DocumentEvent.EventType.CHANGE);
+
+    // Modify the element structure so that the interval begins at an element
+    // start and ends at an element end.
+    buffer.change(offset, length, ev);
+
+    Element root = getDefaultRootElement();
+    // Visit all paragraph elements within the specified interval
+    int paragraphCount =  root.getElementCount();
+    for (int pindex = 0; pindex < paragraphCount; pindex++)
+      {
+	Element paragraph = root.getElement(pindex);
+	// Skip paragraphs that lie outside the interval.
+	if ((paragraph.getStartOffset() > offset + length)
+	    || (paragraph.getEndOffset() < offset))
+	  continue;
+
+	// Visit content elements within this paragraph
+	int contentCount = paragraph.getElementCount();
+	for (int cindex = 0; cindex < contentCount; cindex++)
+	  {
+	    Element content = paragraph.getElement(cindex);
+	    // Skip content that lies outside the interval.
+	    if ((content.getStartOffset() > offset + length)
+		|| (content.getEndOffset() < offset))
+	      continue;
+
+	    if (content instanceof AbstractElement)
+	      {
+		AbstractElement el = (AbstractElement) content;
+		if (replace)
+		  el.removeAttributes(el);
+		el.addAttributes(attributes);
+	      }
+	    else
+	      throw new AssertionError("content elements are expected to be"
+				       + "instances of "
+		       + "javax.swing.text.AbstractDocument.AbstractElement");
+	  }
+      }
   }
   
   public void setLogicalStyle(int position, Style style)
