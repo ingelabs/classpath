11715d09eb1097db4ea7abbff820b419dcc73f3d
Anthony Balkissoon <abalkiss@redhat.com>
2005-08-16 18:07:49 +0000

    2005-08-16  Anthony Balkissoon  <abalkiss@redhat.com>
    	* javax/swing/plaf/basic/BasicTableUI.java:
    	(ActionListenerProxy): Replaced type TableAction with parent type
    	ActionListener to avoid Semantic errors.

--- javax/swing/plaf/basic/BasicTableUI.java
+++ javax/swing/plaf/basic/BasicTableUI.java
@@ -46,6 +46,7 @@ import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 import java.awt.event.InputEvent;
@@ -317,10 +318,10 @@ public class BasicTableUI
   private static class ActionListenerProxy
     extends AbstractAction
   {
-    TableAction target;
+    ActionListener target;
     String bindingCommandName;
 
-    public ActionListenerProxy(TableAction li, 
+    public ActionListenerProxy(ActionListener li, 
                                String cmd)
     {
       target = li;
