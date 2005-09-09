a9e84c3d7d65b34eebc5f01856a058b7ce4b0424
Andrew Haley <aph@redhat.com>
2005-09-09 12:59:53 +0000

    2005-09-09  Andrew Haley  <aph@redhat.com>
    	* java/net/URLClassLoader.java (addURLImpl): Synchronize on the
    	loader.
    	(toString): Likewise.

--- java/net/URLClassLoader.java
+++ java/net/URLClassLoader.java
@@ -701,7 +701,7 @@ public class URLClassLoader extends SecureClassLoader
 
   private void addURLImpl(URL newUrl)
   {
-    synchronized (urlloaders)
+    synchronized (this)
       {
         if (newUrl == null)
           return; // Silently ignore...
@@ -926,7 +926,7 @@ public class URLClassLoader extends SecureClassLoader
    */
   public String toString()
   {
-    synchronized (urlloaders)
+    synchronized (this)
       {
 	if (thisString == null)
 	  {
