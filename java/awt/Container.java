88e2c42c7da20c57684075755f9183ca3ab9a6ae
Roman Kennke <roman@kennke.org>
2005-11-02 18:03:26 +0000

    2005-11-02  Roman Kennke  <kennke@aicas.com>
    	* java/awt/Container.java
    	(addImpl): Notify registered ContainerListeners even when the
    	Container is not showing. Notify the listeners directly, not
    	via the event queue.

--- java/awt/Container.java
+++ java/awt/Container.java
@@ -395,17 +395,20 @@ public class Container extends Component
               layoutMgr.addLayoutComponent(null, comp);
           }
 
-        if (isShowing ())
-          {
-            // Post event to notify of adding the component.
-            ContainerEvent ce = new ContainerEvent(this,
-                                                   ContainerEvent.COMPONENT_ADDED,
-                                                   comp);
-            getToolkit().getSystemEventQueue().postEvent(ce);
+        // We previously only sent an event when this container is showing.
+        // Also, the event was posted to the event queue. A Mauve test shows
+        // that this event is not delivered using the event queue and it is
+        // also sent when the container is not showing. 
+        ContainerEvent ce = new ContainerEvent(this,
+                                               ContainerEvent.COMPONENT_ADDED,
+                                               comp);
+        ContainerListener[] listeners = getContainerListeners();
+        for (int i = 0; i < listeners.length; i++)
+          listeners[i].componentAdded(ce);
 
-            // Repaint this container.
-            repaint();
-          }
+        // Repaint this container.
+        repaint(comp.getX(), comp.getY(), comp.getWidth(),
+                comp.getHeight());
       }
   }
 
