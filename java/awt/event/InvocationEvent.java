5b00bb0cd2d9c9cd4b15e9d301fd4ab9e0d0b01f
Anthony Balkissoon <abalkiss@redhat.com>
2005-11-07 18:28:03 +0000

    2005-11-07  Anthony Balkissoon  <abalkiss@redhat.com>
    	* java/awt/event/InvocationEvent.java:
    	(throwable): New field.
    	(getThrowable): New API method.
    	(dispatch()): Catch Throwable, not Exception.  Save the Throwable.  If
    	it is an Exception, save the Exception.

--- java/awt/event/InvocationEvent.java
+++ java/awt/event/InvocationEvent.java
@@ -107,6 +107,11 @@ public class InvocationEvent extends AWTEvent implements ActiveEvent
   private Exception exception;
 
   /**
+   * This is the caught Throwable thrown in the <code>run()</code> method.
+   */
+  private Throwable throwable;
+  
+  /**
    * The timestamp when this event was created.
    *
    * @see #getWhen()
@@ -183,9 +188,11 @@ public class InvocationEvent extends AWTEvent implements ActiveEvent
         {
           runnable.run();
         }
-      catch (Exception e)
+      catch (Throwable t)
         {
-          exception = e;
+          throwable = t;
+          if (t instanceof Exception)
+            exception = (Exception)t;
         }
     else
       runnable.run();
@@ -210,6 +217,17 @@ public class InvocationEvent extends AWTEvent implements ActiveEvent
     return exception;
   }
 
+  /**
+   * Returns a throwable caught while executing the Runnable's run() method.
+   * Null if none was thrown or if this InvocationEvent doesn't catch
+   * throwables.
+   * @return the caught Throwable
+   */
+  public Throwable getThrowable()
+  {
+    return throwable;
+  }
+  
   /**
    * Gets the timestamp of when this event was created.
    *
