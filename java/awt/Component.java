15f716bbb251d4d7b9685d7f0564781a34d2e308
Anthony Balkissoon <abalkiss@redhat.com>
2005-11-30 19:19:23 +0000

    2005-11-30  Anthony Balkissoon  <abalkiss@redhat.com>
    	Fixes bug #25165
    	* java/awt/Component.java:
    	(processFocusEvent): Don't check if focus opposite is the same as the
    	receiving Component, this is now done in dispatchEventImpl.
    	(dispatchEventImpl): Don't dispatch FocusEvents whose opposite
    	Components are the same.

--- java/awt/Component.java
+++ java/awt/Component.java
@@ -3019,11 +3019,6 @@ public abstract class Component
     if (focusListener == null)
       return;
 
-    // Don't dispatch FOCUS_GAINED or FOCUS_LOST events if the opposite
-    // Component is the same as the receiving Component.
-    if (e.getOppositeComponent() == this)
-      return;
-    
     switch (e.id)
       {
         case FocusEvent.FOCUS_GAINED:
@@ -4796,7 +4791,12 @@ p   * <li>the set of backward traversal keys
   void dispatchEventImpl(AWTEvent e)
   {
     Event oldEvent = translateEvent (e);
-
+    // This boolean tells us not to process focus events when the focus
+    // opposite component is the same as the focus component.
+    boolean ignoreFocus = 
+      (e instanceof FocusEvent && 
+       ((FocusEvent)e).getComponent() == ((FocusEvent)e).getOppositeComponent());
+    
     if (oldEvent != null)
       postEvent (oldEvent);
 
@@ -4827,7 +4827,8 @@ p   * <li>the set of backward traversal keys
                 break;
               }
           }
-        if (e.id != PaintEvent.PAINT && e.id != PaintEvent.UPDATE)
+        if (e.id != PaintEvent.PAINT && e.id != PaintEvent.UPDATE
+            && !ignoreFocus)
           processEvent(e);
       }
 
