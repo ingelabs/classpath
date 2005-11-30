27a59366715531bab063c80fd5b660834bb35f39
Robert Schuster <theBohemian@gmx.net>
2005-11-30 11:17:49 +0000

    Checking the argument is not necessary and in the way it way it was done it was not right.
    2005-11-30  Robert Schuster  <robertschuster@fsfe.org>
    
            * java/beans/PropertyChangeSupport.java:
            (fireIndexedPropertyChange): Removed argument checking.

--- java/beans/PropertyChangeSupport.java
+++ java/beans/PropertyChangeSupport.java
@@ -408,11 +408,9 @@ public class PropertyChangeSupport implements Serializable
   public void fireIndexedPropertyChange(String name, int index,
                                         Object oldValue, Object newValue)
   {
-    // FIXME: should we use equals() here?
-    if (oldValue == newValue && oldValue != null)
+      // Argument checking is done in firePropertyChange(PropertyChangeEvent) .
       firePropertyChange(new IndexedPropertyChangeEvent(source, name,
                                                         oldValue, newValue,
-                                                        index));
   }
 
   /**
