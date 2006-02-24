a345b27a579ae46b6d309ca8940a7d09f508191a
Andrew John Hughes <gnu_andrew@member.fsf.org>
2006-02-24 16:26:59 +0000

    2006-02-24  Andrew John Hughes  <gnu_andrew@member.fsf.org>
    	* gnu/java/security/prng/BasePRNG.java:
    	(clone()): Added cast of buffer to byte[].
    	* gnu/javax/crypto/mac/TMMH16.java:
    	(clone()): Fixed casting of cloned arrays.
    	* native/fdlibm/fdlibm.h:
    	Added missing defines from old fdlibm.h needed by Darwin.
    	(GET_FLOAT_WORD(i,d)): Re-added.
    	(SET_FLOAT_WORD(d,i)): Re-added.

--- native/fdlibm/fdlibm.h
+++ native/fdlibm/fdlibm.h
@@ -248,6 +248,33 @@ extern double __kernel_cos __P((double,double));
 extern double __kernel_tan __P((double,double,int));
 extern int    __kernel_rem_pio2 __P((double*,double*,int,int,int,const int*));
 
+/* A union which permits us to convert between a float and a 32 bit
+   int.  */
+
+typedef union
+{
+  float value;
+  uint32_t word;
+} ieee_float_shape_type;
+
+/* Get a 32 bit int from a float.  */
+
+#define GET_FLOAT_WORD(i,d)					\
+do {								\
+  ieee_float_shape_type gf_u;					\
+  gf_u.value = (d);						\
+  (i) = gf_u.word;						\
+} while (0)
+
+/* Set a float from a 32 bit int.  */
+
+#define SET_FLOAT_WORD(d,i)					\
+do {								\
+  ieee_float_shape_type sf_u;					\
+  sf_u.word = (i);						\
+  (d) = sf_u.value;						\
+} while (0)
+
 #ifdef __cplusplus
 }
 #endif
