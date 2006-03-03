00195976238c84cad2e30169a6727cd0739c5c21
Audrius Meskauskas <audriusa@Bioinformatics.org>
2006-03-03 15:54:41 +0000

    These classes are undocumented in our project. Before working with the code I wrote some comments and applied Eclipse formatter.
    2006-03-03  Audrius Meskauskas  <AudriusA@Bioinformatics.org>
    
        * gnu/java/rmi/dgc/DGCImpl.java,
        java/rmi/dgc/DGC.java,
        java/rmi/dgc/Lease.java: Formatted and commented.

--- gnu/java/rmi/dgc/DGCImpl.java
+++ gnu/java/rmi/dgc/DGCImpl.java
@@ -1,40 +1,41 @@
 /* DGCImpl.java --
-   Copyright (c) 1996, 1997, 1998, 1999, 2002, 2005
-   Free Software Foundation, Inc.
+ Copyright (c) 1996, 1997, 1998, 1999, 2002, 2005
+ Free Software Foundation, Inc.
 
-This file is part of GNU Classpath.
+ This file is part of GNU Classpath.
 
-GNU Classpath is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2, or (at your option)
-any later version.
+ GNU Classpath is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2, or (at your option)
+ any later version.
  
-GNU Classpath is distributed in the hope that it will be useful, but
-WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-General Public License for more details.
+ GNU Classpath is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with GNU Classpath; see the file COPYING.  If not, write to the
-Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-02110-1301 USA.
+ You should have received a copy of the GNU General Public License
+ along with GNU Classpath; see the file COPYING.  If not, write to the
+ Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ 02110-1301 USA.
 
-Linking this library statically or dynamically with other modules is
-making a combined work based on this library.  Thus, the terms and
-conditions of the GNU General Public License cover the whole
-combination.
+ Linking this library statically or dynamically with other modules is
+ making a combined work based on this library.  Thus, the terms and
+ conditions of the GNU General Public License cover the whole
+ combination.
 
-As a special exception, the copyright holders of this library give you
-permission to link this library with independent modules to produce an
-executable, regardless of the license terms of these independent
-modules, and to copy and distribute the resulting executable under
-terms of your choice, provided that you also meet, for each linked
-independent module, the terms and conditions of the license of that
-module.  An independent module is a module which is not derived from
-or based on this library.  If you modify this library, you may extend
-this exception to your version of the library, but you are not
-obligated to do so.  If you do not wish to do so, delete this
-exception statement from your version. */
+ As a special exception, the copyright holders of this library give you
+ permission to link this library with independent modules to produce an
+ executable, regardless of the license terms of these independent
+ modules, and to copy and distribute the resulting executable under
+ terms of your choice, provided that you also meet, for each linked
+ independent module, the terms and conditions of the license of that
+ module.  An independent module is a module which is not derived from
+ or based on this library.  If you modify this library, you may extend
+ this exception to your version of the library, but you are not
+ obligated to do so.  If you do not wish to do so, delete this
+ exception statement from your version. */
+
 
 package gnu.java.rmi.dgc;
 
@@ -49,70 +50,135 @@ import java.rmi.server.RMISocketFactory;
 import java.util.Hashtable;
 
 /**
-  * I let DGCImpl to extend UnicastServerRef, but not 
-  * UnicastRemoteObject, because UnicastRemoteObject must
-  * exportObject automatically.
-  */
+ * The DGC implementation is used for the server side during the distributed
+ * garbage collection. This interface contains the two methods: dirty and clean.
+ * A dirty call is made when a remote reference is unmarshaled in a client. A
+ * corresponding clean call is made by client it no longer uses that remote
+ * reference. A reference to a remote object is also automatically released
+ * after so called lease period that starts after the dirty call is received. It
+ * is the client's responsibility to renew the leases, by making additional
+ * dirty calls before such leases expire.
+ */
 public class DGCImpl
-    extends UnicastServerRef implements DGC {
+    extends UnicastServerRef
+    implements DGC
+{
+  /*
+   * The DGCImpl extends UnicastServerRef and not UnicastRemoteObject, because
+   * UnicastRemoteObject must exportObject automatically.
+   */
 
-    private static final long LEASE_VALUE = 600000L;
-    // leaseCache caches a LeaseRecord associated with a vmid
-    private Hashtable leaseCache = new Hashtable();
+  private static final long LEASE_VALUE = 600000L;
 
-public DGCImpl() throws RemoteException {
-    	super(new ObjID(ObjID.DGC_ID), 0, RMISocketFactory.getSocketFactory());
-}
+  // leaseCache caches a LeaseRecord associated with a vmid
+  private Hashtable leaseCache = new Hashtable();
 
-public Lease dirty(ObjID[] ids, long sequenceNum, Lease lease) throws RemoteException {
-	VMID vmid = lease.getVMID();
-    	if (vmid == null)
-    	    vmid = new VMID();
-    	long leaseValue = LEASE_VALUE;
-    	//long leaseValue = lease.getValue();
+  public DGCImpl() throws RemoteException
+  {
+    super(new ObjID(ObjID.DGC_ID), 0, RMISocketFactory.getSocketFactory());
+  }
+
+  /**
+   * Mark the given objects referecnes as used on the client side.
+   * 
+   * @param ids the ids of the used objects.
+   * @param sequenceNum the number of the call (used to detect and discard late
+   *          calls).
+   * @param lease the requested lease
+   * @return the granted lease
+   */
+  public Lease dirty(ObjID[] ids, long sequenceNum, Lease lease)
+      throws RemoteException
+  {
+    System.out.println("Dirty for "+lease.getValue());
+    VMID vmid = lease.getVMID();
+    if (vmid == null)
+      vmid = new VMID();
+    long leaseValue = LEASE_VALUE;
+    // long leaseValue = lease.getValue();
     lease = new Lease(vmid, leaseValue);
-        synchronized(leaseCache){
-            LeaseRecord lr = (LeaseRecord)leaseCache.get(vmid);
-            if (lr != null)
-                lr.reset(leaseValue);
-            else{
-                lr = new LeaseRecord(vmid, leaseValue);
-                leaseCache.put(vmid, lr);
-            }
-        }
-        
-	return (lease);
-}
+    synchronized (leaseCache)
+      {
+        LeaseRecord lr = (LeaseRecord) leaseCache.get(vmid);
+        if (lr != null)
+          lr.reset(leaseValue);
+        else
+          {
+            lr = new LeaseRecord(vmid, leaseValue);
+            leaseCache.put(vmid, lr);
+          }
+      }
+    return (lease);
+  }
 
-public void clean(ObjID[] ids, long sequenceNum, VMID vmid, boolean strong) throws RemoteException {
-  // Not implemented
-}
-    
+  /**
+   * Mark the given objects as no longer used on the client side.
+   * 
+   * @param ids the ids of the objects that are no longer used.
+   * @param sequenceNum the number of the call (used to detect and discard late
+   *          calls)
+   * @param vmid the VMID of the client.
+   * @param strong make the "strong" clean call.
+   */
+  public void clean(ObjID[] ids, long sequenceNum, VMID vmid, boolean strong)
+      throws RemoteException
+  {
+    // Not implemented
+    // TODO implement
+  }
+
   /**
    * LeaseRecord associates a vmid to expireTime.
    */
-  private static class LeaseRecord{
+  private static class LeaseRecord
+  {
+    /**
+     * The lease id.
+     */
     private VMID vmid;
+
+    /**
+     * The lease expiration time.
+     */ 
     private long expireTime;
-    
-    LeaseRecord(VMID vmid, long leaseValue){
+
+    /**
+     * Create the new lease record.
+     * 
+     * @param vmid lease id.
+     * @param leaseValue lease value
+     */
+    LeaseRecord(VMID vmid, long leaseValue)
+    {
       this.vmid = vmid;
       reset(leaseValue);
     }
-    
-    // reset expireTime
-    void reset(long leaseValue){
+
+    /**
+     * Prolong the expiration time till current time + passed value
+     * 
+     * @param leaseValue the value after that (since the current moment)
+     * the lease should expire in the future.
+     */
+    void reset(long leaseValue)
+    {
       long l = System.currentTimeMillis();
       expireTime = l + leaseValue;
     }
-
-    boolean isExpired(){
+    
+    /**
+     * Check if the lease has been expired.
+     * 
+     * @return true if the lease has been expired, false if it is still valid.
+     */
+    boolean isExpired()
+    {
       long l = System.currentTimeMillis();
-      if ( l > expireTime)
-	return true;
+      if (l > expireTime)
+        return true;
       return false;
     }
-        
-  } //End of LeaseRecord
 
-} //End of DGCImpl
+  } // End of LeaseRecord
+
+} // End of DGCImpl
