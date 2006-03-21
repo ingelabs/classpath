ea3e72dd61331e3b081f9a72b6be659dd100a925
Robert Schuster <theBohemian@gmx.net>
2006-03-21 18:02:45 +0000

    Fixes PR #26677.
    2006-03-21  Robert Schuster  <robertschuster@fsfe.org>
    
            * javax/swing/text/DefaultCaret.java: Added class variable denoting
            the textcomponent having a selection.
            (clearSelection): Clear 'componentWithSelection' variable.
            (handleSelection): Clear selection of current component having a
            selection before setting a new selection in another component.
            (mouseDragged): Only react on left mouse button.
            (mouseClicked): Only react on left mouse button.

--- javax/swing/text/DefaultCaret.java
+++ javax/swing/text/DefaultCaret.java
@@ -63,12 +63,17 @@ import javax.swing.event.EventListenerList;
 /**
  * The default implementation of the {@link Caret} interface.
  *
- * @author orgininal author unknown
+ * @author original author unknown
  * @author Roman Kennke (roman@kennke.org)
  */
 public class DefaultCaret extends Rectangle
   implements Caret, FocusListener, MouseListener, MouseMotionListener
 {
+  
+  /** A text component in the current VM which currently has a
+   * text selection or <code>null</code>.
+   */ 
+  static JTextComponent componentWithSelection;
 
   /**
    * Controls the blinking of the caret.
@@ -346,7 +351,8 @@ public class DefaultCaret extends Rectangle
    */
   public void mouseDragged(MouseEvent event)
   {
-    moveCaret(event);
+    if (event.getButton() == MouseEvent.BUTTON1)
+      moveCaret(event);
   }
 
   /**
@@ -379,7 +385,7 @@ public class DefaultCaret extends Rectangle
   {
     int count = event.getClickCount();
     
-    if (count >= 2)
+    if (event.getButton() == MouseEvent.BUTTON1 && count >= 2)
       {
         int newDot = getComponent().viewToModel(event.getPoint());
         JTextComponent t = getComponent();
@@ -650,6 +656,11 @@ public class DefaultCaret extends Rectangle
           highlightEntry = highlighter.addHighlight(0, 0, getSelectionPainter());
         else
           highlighter.changeHighlight(highlightEntry, 0, 0);
+        
+        // Free the global variable which stores the text component with an active
+        // selection.
+        if (componentWithSelection == textComponent)
+          componentWithSelection = null;
       }
     catch (BadLocationException e)
       {
@@ -685,6 +696,17 @@ public class DefaultCaret extends Rectangle
 	      highlightEntry = highlighter.addHighlight(p0, p1, getSelectionPainter());
 	    else
 	      highlighter.changeHighlight(highlightEntry, p0, p1);
+            
+            // If another component currently has a text selection clear that selection
+            // first.
+            if (componentWithSelection != null)
+              if (componentWithSelection != textComponent)
+                {
+                  Caret c = componentWithSelection.getCaret();
+                  c.setDot(c.getDot());
+                }
+            componentWithSelection = textComponent;
+            
 	  }
 	catch (BadLocationException e)
 	  {
