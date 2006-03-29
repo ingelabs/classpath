d0e8ccffb3181024786ae7356e3384e0397fc382
Robert Schuster <theBohemian@gmx.net>
2006-03-29 14:52:34 +0000

    Fixes PR 26888.
    2006-03-29  Robert Schuster  <robertschuster@fsfe.org>
    
            PR 26888
            * javax/swing/text/GapContent.java:
            (replace): Added call to resetMarksAtZero.

--- javax/swing/text/GapContent.java
+++ javax/swing/text/GapContent.java
@@ -642,6 +642,13 @@ public class GapContent
     if (addItems != null)
       {
         System.arraycopy(addItems, 0, buffer, gapStart, addSize);
+        
+        // Position objects having their mark at the end of the gap
+        // (results in an offset of 0) should be moved down because
+        // the size of the gap will decrease by addSize and the offsets
+        // will increase by the same amount and the latter should not happen.
+        resetMarksAtZero();
+        
         gapStart += addSize;
       }
   }
