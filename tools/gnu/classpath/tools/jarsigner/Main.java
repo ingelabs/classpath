4a078c6f1e89cc93ffee3a622baaf3cf8f09d786
Raif S. Naffah <raif@swiftdsl.com.au>
2006-04-02 01:04:51 +0000

    2006-04-02  Raif S. Naffah  <raif@swiftdsl.com.au>
    	* tools/gnu/classpath/tools/jarsigner/HashUtils.java: Use GPL.
    	* tools/gnu/classpath/tools/jarsigner/SFHelper.java: Likewise.
    	* tools/gnu/classpath/tools/jarsigner/JarVerifier.java: Likewise.
    	* tools/gnu/classpath/tools/jarsigner/JarSigner.java: Likewise.
    	* tools/gnu/classpath/tools/jarsigner/Main.java: Likewise.
    	Re-organised imports.

--- tools/gnu/classpath/tools/jarsigner/Main.java
+++ tools/gnu/classpath/tools/jarsigner/Main.java
@@ -16,27 +16,16 @@ General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with GNU Classpath; see the file COPYING.  If not, write to the
 Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-02110-1301 USA.
-
-Linking this library statically or dynamically with other modules is
-making a combined work based on this library.  Thus, the terms and
-conditions of the GNU General Public License cover the whole
-combination.
-
-As a special exception, the copyright holders of this library give you
-permission to link this library with independent modules to produce an
-executable, regardless of the license terms of these independent
-modules, and to copy and distribute the resulting executable under
-terms of your choice, provided that you also meet, for each linked
-independent module, the terms and conditions of the license of that
-module.  An independent module is a module which is not derived from
-or based on this library.  If you modify this library, you may extend
-this exception to your version of the library, but you are not
-obligated to do so.  If you do not wish to do so, delete this
-exception statement from your version. */
+02110-1301 USA. */
 
 
 package gnu.classpath.tools.jarsigner;
+
+import gnu.classpath.SystemProperties;
+import gnu.classpath.tools.HelpPrinter;
+import gnu.java.security.OID;
+import gnu.java.security.Registry;
+import gnu.javax.security.auth.callback.ConsoleCallbackHandler;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -61,12 +50,6 @@ import javax.security.auth.callback.Callback;
 import javax.security.auth.callback.CallbackHandler;
 import javax.security.auth.callback.PasswordCallback;
 import javax.security.auth.callback.UnsupportedCallbackException;
-
-import gnu.classpath.SystemProperties;
-import gnu.classpath.tools.HelpPrinter;
-import gnu.java.security.OID;
-import gnu.java.security.Registry;
-import gnu.javax.security.auth.callback.ConsoleCallbackHandler;
 
 /**
  * The GNU Classpath implementation of the <i>jarsigner</i> tool.
