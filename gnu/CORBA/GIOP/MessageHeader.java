25d0f4e0b5c5394910f3a04a62feb10005ce45fe
Audrius Meskauskas <audriusa@Bioinformatics.org>
2006-04-09 12:01:59 +0000

    2006-04-09  Audrius Meskauskas  <AudriusA@Bioinformatics.org>
    	* gnu/CORBA/GIOP/MessageHeader.java (write):
    	More informative exception.
    	* gnu/CORBA/IorDelegate.java (release): Do not close the socket.
    	* gnu/CORBA/SocketRepository.java (get_socket):
    	Removed debugging code.

--- gnu/CORBA/GIOP/MessageHeader.java
+++ gnu/CORBA/GIOP/MessageHeader.java
@@ -371,7 +371,7 @@ public class MessageHeader
       }
     catch (IOException ex)
       {
-        MARSHAL t = new MARSHAL();
+        MARSHAL t = new MARSHAL(ex.getMessage());
         t.minor = Minor.Header;
         t.initCause(ex);
         throw t;
