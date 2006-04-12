b90cc775e4018e9eb6f9c9edafcdb91b33272f04
Mark Wielaard <mark@klomp.org>
2006-04-12 12:04:16 +0000

    2006-04-12  Mark Wielaard  <mark@klomp.org>
           Port UncaughtExceptionHandler support from generics branch.
           * NEWS: Document Thread.UncaughtExceptionHandler VMThread change.
    
    2006-04-12  Andrew John Hughes  <gnu_andrew@member.fsf.org>
    
           * java/lang/Thread.java:
           (setUncaughtExceptionHandler(UncaughtExceptionHandler):
           Added docs and security check.
           (getUncaughtExceptionHandler()): Documented.
           (setDefaultUncaughtExceptionHandler(UncaughtExceptionHandler):
           Added docs and security check.
           (getDefaultUncaughtExceptionHandler()): Documented.
           (getId()): Documented.
    
    2006-04-12  Tom Tromey  <tromey@redhat.com>
    
           * vm/reference/java/lang/VMThread.java (run): Use thread's
           uncaught handler.
           * java/lang/Thread.java (defaultHandler): New field.
           (setDefaultUncaughtExceptionHandler,
           getDefaultUncaughtExceptionHandler, setUncaughtExceptionHandler,
           getUncaughtExceptionHandler): New methods.
           * java/lang/ThreadGroup.java (ThreadGroup): Implements
           UncaughtExceptionHandler.
           (uncaughtException): Use getDefaultUncaughtExceptionHandler.

--- vm/reference/java/lang/VMThread.java
+++ vm/reference/java/lang/VMThread.java
@@ -123,7 +123,10 @@ final class VMThread
 	    {
 		try
 		{
-		    thread.group.uncaughtException(thread, t);
+		  Thread.UncaughtExceptionHandler handler = thread.getUncaughtExceptionHandler();
+		  if (handler == null)
+		    handler = thread.group;
+		  handler.uncaughtException(thread, t);
 		}
 		catch(Throwable ignore)
 		{
