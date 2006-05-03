e5d30b58c24d0a372635fca11959f72cc84169fc
Robert Schuster <theBohemian@gmx.net>
2006-05-03 18:11:05 +0000

    Fixes PR #27393.
    2006-05-03  Robert Schuster  <robertschuster@fsfe.org>
    
            * javax/swing/plaf/basic/BasicTextUI.java:
            (changeUpdate): Added note.
            (removeUpdate): Dito.
            (insertUpdate): Dito.
            (damageRange): Added if-block to return early.
            (modelToView): Added check of getVisibleEditorRect's return value.
            (getVisibleEditorRect): Return null instead of empty rectangle.
            * javax/swing/text/DefaultCaret.java:
            (clearHighlight): Removed if-clause to create a highlight entry if it
            did not exist before.
            * javax/swing/text/WrappedPlainView.java:
            (WrappedLine.modelToView): Throw exception if allocation area is empty,
            removed 2nd part of if-expression.
            (WrappedLine.updateDamage): Added more documentation, added check
            whether allocation area rectangle is null.

--- javax/swing/text/WrappedPlainView.java
+++ javax/swing/text/WrappedPlainView.java
@@ -542,9 +542,15 @@ public class WrappedPlainView extends BoxView implements TabExpander
     public Shape modelToView(int pos, Shape a, Bias b)
         throws BadLocationException
     {
+      Rectangle rect = a.getBounds();
+      
+      // Throwing a BadLocationException is an observed behavior of the RI.
+      if (rect.isEmpty())
+        throw new BadLocationException("Unable to calculate view coordinates "
+                                       + "when allocation area is empty.", 5);
+      
       Segment s = getLineBuffer();
       int lineHeight = metrics.getHeight();
-      Rectangle rect = a.getBounds();
       
       // Return a rectangle with width 1 and height equal to the height 
       // of the text
@@ -563,8 +569,7 @@ public class WrappedPlainView extends BoxView implements TabExpander
           // If pos is between currLineStart and currLineEnd then just find
           // the width of the text from currLineStart to pos and add that
           // to rect.x
-          if (pos >= currLineStart && pos < currLineEnd || 
-              (currLineEnd==currLineStart) )            
+          if (pos >= currLineStart && pos < currLineEnd)
             {             
               try
                 {
@@ -666,11 +671,16 @@ public class WrappedPlainView extends BoxView implements TabExpander
     }    
     
     /**
-     * This method is called from insertUpdate and removeUpdate.
-     * If the number of lines in the document has changed, just repaint
+     * <p>This method is called from insertUpdate and removeUpdate.</p>
+     * 
+     * <p>If the number of lines in the document has changed, just repaint
      * the whole thing (note, could improve performance by not repainting 
      * anything above the changes).  If the number of lines hasn't changed, 
-     * just repaint the given Rectangle.
+     * just repaint the given Rectangle.</p>
+     * 
+     * <p>Note that the <code>Rectangle</code> argument may be <code>null</code>
+     * when the allocation area is empty.</code> 
+     * 
      * @param a the Rectangle to repaint if the number of lines hasn't changed
      */
     void updateDamage (Rectangle a)
@@ -679,7 +689,7 @@ public class WrappedPlainView extends BoxView implements TabExpander
       // As determining the number of lines is impossible in that state we
       // reset it to an invalid value which can then be recalculated at a
       // later point.
-      if (a.isEmpty())
+      if (a == null || a.isEmpty())
         {
           numLines = 1;
           return;
