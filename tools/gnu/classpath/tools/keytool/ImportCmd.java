02de0247da589918b7b7ee5230f61aa6e27ba671
Raif S. Naffah <raif@swiftdsl.com.au>
2006-06-01 19:50:36 +0000

    2006-06-02  Raif S. Naffah  <raif@swiftdsl.com.au>
    	* tools/gnu/classpath/tools/keytool/StorePasswdCmd.java: Fixed typo.
    	* tools/gnu/classpath/tools/keytool/CertReqCmd.java: Likewise.
    	* tools/gnu/classpath/tools/keytool/DeleteCmd.java: Likewise.
    	* tools/gnu/classpath/tools/keytool/ExportCmd.java: Likewise.
    	* tools/gnu/classpath/tools/keytool/GenKeyCmd.java: Likewise.
    	* tools/gnu/classpath/tools/keytool/IdentityDBCmd.java: Likewise.
    	* tools/gnu/classpath/tools/keytool/ImportCmd.java: Likewise.
    	* tools/gnu/classpath/tools/keytool/KeyCloneCmd.java: Likewise.
    	* tools/gnu/classpath/tools/keytool/KeyPasswdCmd.java: Likewise.
    	* tools/gnu/classpath/tools/keytool/ListCmd.java: Likewise.
    	* tools/gnu/classpath/tools/keytool/SelfCertCmd.java: Likewise.

--- tools/gnu/classpath/tools/keytool/ImportCmd.java
+++ tools/gnu/classpath/tools/keytool/ImportCmd.java
@@ -148,7 +148,7 @@ import javax.security.auth.callback.UnsupportedCallbackException;
  *      should be considered when trying to establish chain-of-trusts.
  *      <p></dd>
  *      
- *      <dt>-storetype STORE_TYP}</dt>
+ *      <dt>-storetype STORE_TYPE</dt>
  *      <dd>Use this option to specify the type of the key store to use. The
  *      default value, if this option is omitted, is that of the property
  *      <code>keystore.type</code> in the security properties file, which is
