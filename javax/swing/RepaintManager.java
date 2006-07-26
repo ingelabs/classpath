573c7e46496811586ac0422956846ee61ed160f9
Roman Kennke <roman@kennke.org>
2006-07-26 21:16:51 +0000

    2006-07-26  Roman Kennke  <kennke@aicas.com>
    	* javax/swing/RepaintManager.java
    	(markCompletelyDirty): Add dirty region with Integer.MAX_VALUE
    	for the component.
    	(isCompletelyDirty): Consider a component completely dirty
    	when it has a dirty region with Integer.MAX_VALUE.

--- javax/swing/RepaintManager.java
+++ javax/swing/RepaintManager.java
@@ -462,8 +462,7 @@ public class RepaintManager
    */
   public void markCompletelyDirty(JComponent component)
   {
-    Rectangle r = component.getBounds();
-    addDirtyRegion(component, 0, 0, r.width, r.height);
+    addDirtyRegion(component, 0, 0, Integer.MAX_VALUE, Integer.MAX_VALUE);
   }
 
   /**
@@ -501,13 +500,11 @@ public class RepaintManager
    */
   public boolean isCompletelyDirty(JComponent component)
   {
-    boolean retVal = false;
-    if (dirtyComponents.containsKey(component))
-      {
-        Rectangle dirtyRegion = (Rectangle) dirtyComponents.get(component);
-        retVal = dirtyRegion.equals(SwingUtilities.getLocalBounds(component));
-      }
-    return retVal;
+    boolean dirty = false;
+    Rectangle r = getDirtyRegion(component);
+    if(r.width == Integer.MAX_VALUE && r.height == Integer.MAX_VALUE)
+      dirty = true;
+    return dirty;
   }
 
   /**
