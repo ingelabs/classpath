9a5da38f0c2bad95d1ea50346b93d5f22f5f2d4f
Robert Schuster <theBohemian@gmx.net>
2006-07-26 18:20:11 +0000

    2006-07-26  Robert Schuster <robertschuster@fsfe.org>
            * gnu/java/awt/peer/gtk/CairoGraphics2D.java:
            (drawLine): Apply shift to line coordinates.

--- gnu/java/awt/peer/gtk/CairoGraphics2D.java
+++ gnu/java/awt/peer/gtk/CairoGraphics2D.java
@@ -1044,7 +1044,7 @@ public abstract class CairoGraphics2D extends Graphics2D
     if (x1 == x2 && y1 == y2)
       cairoFillRect(nativePointer, x1, y1, 1, 1);
     else
-      cairoDrawLine(nativePointer, x1, y1, x2, y2);
+      cairoDrawLine(nativePointer, x1 + 0.5, y1 + 0.5, x2 + 0.5, y2 + 0.5);
   }
 
   public void drawRect(int x, int y, int width, int height)
