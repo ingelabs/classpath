21e63175368553096eeacd9da997084455d6584f
Roman Kennke <roman@kennke.org>
2006-10-18 18:46:25 +0000

    2006-10-18  Roman Kennke  <kennke@aicas.com>
    	PR 28769
    	* javax/swing/JScrollPane.java
    	(viewportBorder): Made field private.
    	(wheelScrollingEnabled): Made field private.
    	(JScrollPane): Enabled wheel scrolling by default.
    	* javax/swing/JTree.java
    	(TreeSelectionRedirector.valueChanged): Don't repaint anything
    	here.
    	(getScrollableUnitIncrement): Fixed thinko.
    	* javax/swing/plaf/basic/BasicScrollBarUI.java
    	(static scrollByBlock): New static method to avoid code duplication
    	for the BasicScrollPane wheel scrolling.
    	(static scrollByUnits): New static method to avoid code duplication
    	for the BasicScrollPane wheel scrolling.
    	(scrollByBlock): Delegate to static helper method.
    	(scrollByUnit): Delegate to static helper method.
    	* javax/swing/plaf/basic/BasicScrollPaneUI.java
    	(MouseWheelHandler.mouseWheelMoved): Delegate to BasicScrollBarUI
    	static helper methods to avoid code duplication.
    	(MouseWheelHandler.bounds): Removed.
    	(MouseWheelHandler.getValue): Removed.
    	(MouseWheelHandler.scroll): Removed.

--- javax/swing/JTree.java
+++ javax/swing/JTree.java
@@ -1279,13 +1279,6 @@ public class JTree extends JComponent implements Scrollable, Accessible
       TreeSelectionEvent rewritten = 
         (TreeSelectionEvent) ev.cloneWithSource(JTree.this);
       fireValueChanged(rewritten);
-
-      // Only repaint the changed nodes.
-      TreePath[] changed = ev.getPaths();
-      for (int i = 0; i < changed.length; i++)
-        {
-          repaint(getPathBounds(changed[i]));
-        }
     }
   }
 
@@ -1698,7 +1691,7 @@ public class JTree extends JComponent implements Scrollable, Accessible
                 if (direction < 0)
                   delta = Math.max(0, visibleRect.y - b.y);
                 else
-                  delta = b.y + b.height - visibleRect.height;
+                  delta = b.y + b.height - visibleRect.y;
               }
             else
               {
