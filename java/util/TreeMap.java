f765c5a58eb9d1a38ccccacc10c7f49aab4a706e
Tom Tromey <tromey@redhat.com>
2006-12-13 20:29:49 +0000

    * java/security/BasicPermission.java: Remove gcj workaround. 	* java/security/cert/X509Certificate.java: Remove gcj workaround. 	* java/net/ServerSocket.java (ServerSocket): Remove gcj 	workaround. 	(getImpl): Likewise. 	* java/util/TreeMap.java (TreeIterator): Remove gcj workaround. 	* java/text/AttributedStringIterator.java (getRunLimit): Remove 	qualifications. 	(getRunStart): Likewise. 	* java/awt/AWTKeyStroke.java (removeEldestEntry): Remove gcj 	workaround. 	* java/awt/AlphaComposite.java (removeEldestEntry): Remove gcj 	workaround. 	* java/awt/geom/GeneralPath.java (WIND_EVEN_ODD, WIND_NON_ZERO, 	BIG_VALUE): Don't fully qualify. 	* gnu/javax/sound/midi/alsa/AlsaMidiSequencerDevice.java: Removed 	imports. 	* gnu/javax/sound/midi/alsa/AlsaPortDevice.java: Removed import. 	* gnu/javax/sound/midi/dssi/DSSISynthesizer.java: Removed import. 	* javax/swing/JComponent.java (firePropertyChange): Removed. 	* javax/swing/text/InternationalFormatter.java (clone): 	Uncomment. 	* javax/swing/text/AbstractDocument.java (addEdit): Remove gcj 	workaround. 	(getChange): Likewise.

--- java/util/TreeMap.java
+++ java/util/TreeMap.java
@@ -1,6 +1,6 @@
 /* TreeMap.java -- a class providing a basic Red-Black Tree data structure,
    mapping Object --> Object
-   Copyright (C) 1998, 1999, 2000, 2001, 2002, 2004, 2005  Free Software Foundation, Inc.
+   Copyright (C) 1998, 1999, 2000, 2001, 2002, 2004, 2005, 2006  Free Software Foundation, Inc.
 
 This file is part of GNU Classpath.
 
@@ -1409,11 +1409,7 @@ public class TreeMap<K, V> extends AbstractMap<K, V>
      */
     TreeIterator(int type)
     {
-      // FIXME gcj cannot handle this. Bug java/4695
-      // this(type, firstNode(), nil);
-      this.type = type;
-      this.next = firstNode();
-      this.max = nil;
+      this(type, firstNode(), nil);
     }
 
     /**
