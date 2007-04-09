54ab2274d08f26ca2145e799f7908824c103fab9
Francis Kung <fkung@redhat.com>
2007-04-09 21:34:27 +0000

    2007-04-09  Francis Kung  <fkung@redhat.com>
    	PR 31311
    	* gnu/java/awt/peer/gtk/ComponentGraphics.java
    	(dispose): Removed method.
    	(disposeSurface): Removed method.
    	* gnu_java_awt_peer_gtk_ComponentGraphics.h: Regenerated.
    	* native/jni/gtk-peer/gnu_java_awt_peer_gtk_ComponentGraphics.c
    	(Java_gnu_java_awt_peer_gtk_ComponentGraphics_disposeSurface): Removed.
    	(Java_gnu_java_awt_peer_gtk_ComponentGraphics_initState): Destroy surface
    	after it is used to create a cairo context.

--- native/jni/gtk-peer/gnu_java_awt_peer_gtk_ComponentGraphics.c
+++ native/jni/gtk-peer/gnu_java_awt_peer_gtk_ComponentGraphics.c
@@ -159,38 +159,11 @@ Java_gnu_java_awt_peer_gtk_ComponentGraphics_initState
 
   cr = cairo_create (surface);
   g_assert(cr != NULL);
+  cairo_surface_destroy(surface);
 
   gdk_threads_leave();
 
   return PTR_TO_JLONG(cr);
-}
-
-/**
- * Disposes of the surface
- */
-JNIEXPORT void JNICALL
-Java_gnu_java_awt_peer_gtk_ComponentGraphics_disposeSurface
-  (JNIEnv *env __attribute__((unused)), jobject obj __attribute__((unused)),
-   jlong value)
-{
-  struct cairographics2d *gr;
-  cairo_surface_t *surface;
-
-  gr = JLONG_TO_PTR(struct cairographics2d, value);
-
-  if (gr == NULL)
-    return;
-
-  if (gr->cr == NULL)
-    return;
-
-  surface = cairo_get_target (gr->cr);
-  if (surface != NULL)
-    {
-      gdk_threads_enter();
-      cairo_surface_destroy (surface);
-      gdk_threads_leave();
-    }
 }
 
 JNIEXPORT jlong JNICALL 
