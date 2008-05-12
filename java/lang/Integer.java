02921b01621bc91bc1c1b9729e33515fea757e59
Andrew John Hughes <gnu_andrew@member.fsf.org>
2008-05-12 22:14:55 +0000

    2008-05-11  Andrew John Hughes  <gnu_andrew@member.fsf.org>
            Reported by Nicolas Geoffray <nicolas.geoffray@menlina.com>
    	* java/lang/Integer.java:
    	(parseInt(String,int,boolean)): Parse +x
    	as x, not -x.

--- java/lang/Integer.java
+++ java/lang/Integer.java
@@ -776,15 +776,18 @@ public final class Integer extends Number implements Comparable<Integer>
     if (len == 0)
       throw new NumberFormatException("string length is null");
     int ch = str.charAt(index);
-    if (ch == '-' || ch == '+')
+    if (ch == '-')
       {
         if (len == 1)
-          if (ch == '-')
-            throw new NumberFormatException("pure '-'");
-          else if (ch == '+')
-            throw new NumberFormatException("pure '+'");
+	  throw new NumberFormatException("pure '-'");
         isNeg = true;
         ch = str.charAt(++index);
+      }
+    if (ch == '+')
+      {
+	if (len == 1)
+	  throw new NumberFormatException("pure '+'");
+	ch = str.charAt(++index);
       }
     if (decode)
       {
