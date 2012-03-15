a1e2fbeff01860fc2c7ad5c8a0a6982e5ca14b95
Pekka Enberg <penberg@kernel.org>
2012-03-15 22:13:03 +0200

    Fix java/lang/String.codePoint{At|Before} exception types
    OpenJDK throws StringIndexOutOfBoundsException and so should we.
    
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

--- java/lang/String.java
+++ java/lang/String.java
@@ -705,6 +705,8 @@ public final class String
    */
   public synchronized int codePointAt(int index)
   {
+    if (index < 0 || index >= count)
+      throw new StringIndexOutOfBoundsException(index);
     // Use the CharSequence overload as we get better range checking
     // this way.
     return Character.codePointAt(this, index);
@@ -722,6 +724,8 @@ public final class String
    */
   public synchronized int codePointBefore(int index)
   {
+    if (index < 0 || index >= count)
+      throw new StringIndexOutOfBoundsException(index);
     // Use the CharSequence overload as we get better range checking
     // this way.
     return Character.codePointBefore(this, index);
